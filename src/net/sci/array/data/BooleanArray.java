f0273e798daa4bcdd8a6b65cb3add925914506dc
dlegland <david.legland@nantes.inra.fr>
2016-09-08 18:38:23 +0200

    add Boolean data type and Boolean arrays

---
+++ src/net/sci/array/data/BooleanArray.java
@@ -0,0 +1,101 @@
+/**
+ * 
+ */
+package net.sci.array.data;
+
+import net.sci.array.data.scalar2d.BooleanArray2D;
+import net.sci.array.type.Boolean;
+
+/**
+ * @author dlegland
+ *
+ */
+public interface BooleanArray extends IntArray<Boolean>
+{
+	// =============================================================
+	// Static methods
+
+	public static BooleanArray create(int[] dims)
+	{
+		switch (dims.length)
+		{
+		case 2:
+			return BooleanArray2D.create(dims[0], dims[1]);
+//		case 3:
+//			return UInt8Array3D.create(dims[0], dims[1], dims[2]);
+		default:
+			throw new RuntimeException("Cound not create Boolean array of dimension " + dims.length);
+//			return UInt8ArrayND.create(dims);
+		}
+	}
+	
+	// =============================================================
+	// New methods
+
+	public boolean getState(int[] pos);
+	
+	public void setState(int[] pos, boolean state);
+	
+	
+	// =============================================================
+	// Specialization of the IntArray interface
+
+	@Override
+	public default int getInt(int[] pos)
+	{
+		return getState(pos) ? 1 : 0; 
+	}
+
+	@Override
+	public default void setInt(int[] pos, int value)
+	{
+		setState(pos, value != 0);
+	}
+
+	
+	// =============================================================
+	// Specialization of the Array interface
+
+	@Override
+	public default BooleanArray newInstance(int... dims)
+	{
+		return BooleanArray.create(dims);
+	}
+
+	@Override
+	public BooleanArray duplicate();
+
+	public Iterator iterator();
+	
+	/**
+	 * Sets the value at the specified position, by clamping the value between 0
+	 * and 255.
+	 */
+	public default void setValue(int[] pos, double value)
+	{
+		setInt(pos, (int) Math.min(Math.max(value, 0), 255));
+	}
+
+
+	// =============================================================
+	// Inner interface
+
+	public interface Iterator extends IntArray.Iterator<Boolean>
+	{
+		public boolean getState();
+		public void setState(boolean b);
+		
+		@Override
+		public default int getInt()
+		{
+			return getState() ? 1 : 0; 
+		}
+
+		@Override
+		public default void setInt(int value)
+		{
+			setState(value > 0);
+		}
+
+	}
+}
---
+++ src/net/sci/array/data/scalar2d/BufferedBooleanArray2D.java
@@ -0,0 +1,136 @@
+/**
+ * 
+ */
+package net.sci.array.data.scalar2d;
+
+import net.sci.array.data.BooleanArray;
+import net.sci.array.type.Boolean;
+
+/**
+ * @author dlegland
+ *
+ */
+public class BufferedBooleanArray2D extends BooleanArray2D
+{
+	// =============================================================
+	// Class fields
+
+	boolean[] buffer;
+
+	
+	// =============================================================
+	// Constructors
+
+	/**
+	 * @param size0
+	 * @param size1
+	 */
+	public BufferedBooleanArray2D(int size0, int size1)
+	{
+		super(size0, size1);
+		this.buffer = new boolean[size0 * size1];
+	}
+
+	public BufferedBooleanArray2D(int size0, int size1, boolean[] buffer)
+	{
+		super(size0, size1);
+		if (buffer.length < size0 * size1)
+		{
+			throw new IllegalArgumentException("Buffer size does not match image dimensions");
+		}
+		this.buffer = buffer;
+	}
+
+	// =============================================================
+	// Implementation of the BooleanArray2D interface
+
+	/* (non-Javadoc)
+	 * @see net.sci.array.data.scalar2d.BooleanArray2D#getState(int, int)
+	 */
+	@Override
+	public boolean getState(int x, int y)
+	{
+		int index = x + y * this.size0;
+		return buffer[index];
+	}
+
+	/* (non-Javadoc)
+	 * @see net.sci.array.data.scalar2d.BooleanArray2D#setState(int, int, boolean)
+	 */
+	@Override
+	public void setState(int x, int y, boolean state)
+	{
+		int index = x + y * this.size0;
+		buffer[index] = state;
+	}
+
+	// =============================================================
+	// Implementation of the Array interface
+
+	/* (non-Javadoc)
+	 * @see net.sci.array.data.scalar2d.BooleanArray2D#duplicate()
+	 */
+	@Override
+	public BooleanArray2D duplicate()
+	{
+		int n = this.size0 * this.size1;
+		boolean[] buffer2 = new boolean[n];
+		System.arraycopy(this.buffer, 0, buffer2, 0, n);
+		return new BufferedBooleanArray2D(this.size0, this.size1, buffer2);
+	}
+
+	/* (non-Javadoc)
+	 * @see net.sci.array.data.BooleanArray#iterator()
+	 */
+	@Override
+	public net.sci.array.data.BooleanArray.Iterator iterator()
+	{
+		return new BooleanIterator();
+	}
+
+	private class BooleanIterator implements BooleanArray.Iterator
+	{
+		int index = -1;
+		
+		public BooleanIterator() 
+		{
+		}
+		
+		@Override
+		public boolean hasNext()
+		{
+			return this.index < (size0 * size1 - 1);
+		}
+
+		@Override
+		public Boolean next()
+		{
+			this.index++;
+			return new Boolean(buffer[index]);
+		}
+
+		@Override
+		public void forward()
+		{
+			this.index++;
+		}
+
+		@Override
+		public Boolean get()
+		{
+			return new Boolean(buffer[index]);
+		}
+
+		@Override
+		public boolean getState()
+		{
+			return buffer[index];
+		}
+
+		@Override
+		public void setState(boolean b)
+		{
+			buffer[index] = b;
+		}
+	}
+}
