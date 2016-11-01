9dc279f444228ac51cc7a918542ef69afac4adbf
dlegland <david.legland@nantes.inra.fr>
2016-11-01 12:59:00 +0100

    work on generic operators

--- src/main/java/net/sci/array/process/shape/Slicer.java
+++ src/main/java/net/sci/array/process/shape/Slicer.java
@@ -54,11 +54,24 @@ public class Slicer implements ArrayOperator
 	}
 
 	@Override
+	public <T> Array<?> process(Array<T> source)
+	{
+		Array<T> target = source.newInstance(source.getSize());
+		processNd(source, target);
+		return target;
+	}
+	
+	@Override
 	public void process(Array<?> source, Array<?> target)
 	{
 		// TODO: check dims
-		
 		processDoubleNd(source, target);
+	}
+	
+	public <T> void processTyped(Array<? extends T> source, Array<? super T> target)
+	{
+		// TODO: check dims
+		processNd(source, target);
 	}
 	
 	/**
@@ -115,6 +128,30 @@ public class Slicer implements ArrayOperator
 			
 			// copy value of selected position
 			target.setValue(pos, source.getValue(srcPos));
+		}
+	}
+	
+	public <T> void processNd(Array<T> source, Array<? super T> target)
+	{
+		// create position pointer for source image
+		int nd = target.dimensionality();
+		int[] srcPos = new int[nd + 1];
+		srcPos[this.dim] = this.index;
+
+		// iterate over position in target image
+		Array.Cursor cursor = target.getCursor();
+		while (cursor.hasNext())
+		{
+			// get position in target image
+			cursor.forward();
+			int[] pos = cursor.getPosition();
+			
+			// convert to position in source image
+			System.arraycopy(pos, 0, srcPos, 0, dim);
+			System.arraycopy(pos, dim, srcPos, dim + 1, nd - dim);
+			
+			// copy value of selected position
+			target.set(pos, source.get(srcPos));
 		}
 	}
 
