bdd142b691a00be0ff65118277cb4688b3cf7920
dlegland <david.legland@nantes.inra.fr>
2016-11-02 15:40:36 +0100

    add RGB8Array split/merge channels

--- src/main/java/net/sci/array/data/color/RGB8Array.java
+++ src/main/java/net/sci/array/data/color/RGB8Array.java
@@ -3,6 +3,10 @@
  */
 package net.sci.array.data.color;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
+import net.sci.array.data.UInt8Array;
 import net.sci.array.data.VectorArray;
 import net.sci.array.type.RGB8;
 
@@ -31,6 +35,67 @@ public interface RGB8Array extends VectorArray<RGB8>
 //			return UInt8ArrayND.create(dims);
 		}
 	}
+
+	public static Collection<UInt8Array> splitChannels(RGB8Array array)
+	{
+		// create result arrays
+		int[] dims = array.getSize();
+		UInt8Array redChannel = UInt8Array.create(dims);
+		UInt8Array greenChannel = UInt8Array.create(dims);
+		UInt8Array blueChannel = UInt8Array.create(dims);
+		
+		// create iterators
+		Iterator rgbIter = array.iterator();
+		UInt8Array.Iterator rIter = redChannel.iterator();
+		UInt8Array.Iterator gIter = greenChannel.iterator();
+		UInt8Array.Iterator bIter = blueChannel.iterator();
+		
+		// iterate over elements of all arrays simultaneously
+		while (rgbIter.hasNext())
+		{
+			rgbIter.forward();
+			RGB8 rgb = rgbIter.get();
+			rIter.forward();
+			rIter.setInt(rgb.getSample(0));
+			gIter.forward();
+			gIter.setInt(rgb.getSample(1));
+			bIter.forward();
+			bIter.setInt(rgb.getSample(2));
+		}
+		
+		// create the collection of channels
+		Collection<UInt8Array> channels = new ArrayList<>(3);
+		channels.add(redChannel);
+		channels.add(greenChannel);
+		channels.add(blueChannel);
+		
+		return channels;
+	}
+	
+	public static RGB8Array mergeChannels(UInt8Array redChannel, UInt8Array greenChannel, UInt8Array blueChannel)
+	{
+		int[] dims = redChannel.getSize();
+		RGB8Array result = create(dims);
+		
+		// get iterators
+		Iterator rgbIter = result.iterator();
+		UInt8Array.Iterator rIter = redChannel.iterator();
+		UInt8Array.Iterator gIter = greenChannel.iterator();
+		UInt8Array.Iterator bIter = blueChannel.iterator();
+		
+		// iterate over elements of all arrays simultaneously
+		while (rgbIter.hasNext())
+		{
+			int r = rIter.next().getInt();
+			int g = gIter.next().getInt();
+			int b = bIter.next().getInt();
+			rgbIter.forward();
+			rgbIter.set(new RGB8(r, g, b));
+		}
+		
+		return result;
+	}
+	
 
 	// =============================================================
 	// Specialization of VectorArray interface
