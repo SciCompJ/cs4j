4f513e4ddb8700b5db209c0777f05aecd180e5b2
dlegland <david.legland@inra.fr>
2016-11-17 12:20:23 +0100

    update tests

---
+++ src/test/java/net/sci/array/data/scalarnd/AllTests.java
@@ -0,0 +1,14 @@
+package net.sci.array.data.scalarnd;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	// generic classes
+	BufferedInt32ArrayNDTest.class,
+	BufferedUInt8ArrayNDTest.class,
+	})
+public class AllTests {
+  //nothing
+}
---
+++ src/test/java/net/sci/image/morphology/AllTests.java
@@ -0,0 +1,14 @@
+package net.sci.image.morphology;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	// generic classes
+	FloodFill2DTest.class,
+	FloodFill2DTest.class,
+	})
+public class AllTests {
+  //nothing
+}
