baa0e1b35cabe214c46b65a757a92b46b89a3cce
dlegland <david.legland@nantes.inra.fr>
2017-02-12 20:29:02 +0100

    clean up code

--- src/test/java/net/sci/table/io/DelimitedTableWriterTest.java
+++ src/test/java/net/sci/table/io/DelimitedTableWriterTest.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 
 import net.sci.table.DataTable;
+import net.sci.table.Table;
 
 import org.junit.Test;
 
@@ -13,9 +14,14 @@ public class DelimitedTableWriterTest
 	@Test
 	public final void testWriteTable() throws IOException
 	{
+		Table table = new DataTable(5, 3);
+		table.setColumnNames(new String[]{"col1", "col2", "col3"});
+		table.setRowNames(new String[]{"row1", "row2", "row3", "row4", "row5"});
+		
 		File file = new File("output.txt");
 		DelimitedTableWriter tw = new DelimitedTableWriter(file);
-		tw.writeTable(new DataTable(5, 3));
+		
+		tw.writeTable(table);
 	}
 
 }
