d1e6e545480cebce992d42670b16c6e24b5642bc
dlegland <david.legland@nantes.inra.fr>
2017-06-29 10:20:18 +0200

    add box variance filter

---
+++ src/main/java/net/sci/image/process/filter/BoxVarianceFilter.java
@@ -0,0 +1,278 @@
+/**
+ * 
+ */
+package net.sci.image.process.filter;
+
+import net.sci.algo.AlgoStub;
+import net.sci.array.Array;
+import net.sci.array.Cursor;
+import net.sci.array.data.Float64Array;
+import net.sci.array.data.ScalarArray;
+import net.sci.array.data.scalar2d.ScalarArray2D;
+import net.sci.array.data.scalar3d.ScalarArray3D;
+import net.sci.image.ArrayToArrayImageOperator;
+
+/**
+ * Computes the variance in a box neighborhood around each array element.
+ * 
+ * @author dlegland
+ *
+ */
+public final class BoxVarianceFilter extends AlgoStub implements ArrayToArrayImageOperator
+{
+	int[] radiusList;
+	
+	
+	/**
+	 * Creates a new instance of box filter by specifying the list of radius in
+	 * each dimension.
+	 * 
+	 * @param radiusList
+	 *            the box radius in each dimension
+	 */
+	public BoxVarianceFilter(int[] radiusList)
+	{
+		this.radiusList = new int[radiusList.length];
+		for (int i = 0; i < radiusList.length; i++)
+		{
+			this.radiusList[i] = radiusList[i];
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see net.sci.array.ArrayOperator#process(net.sci.array.Array, net.sci.array.Array)
+	 */
+	@Override
+	public void process(Array<?> source, Array<?> target)
+	{
+		// Choose the best possible implementation, depending on array dimensions
+		if (source instanceof ScalarArray2D && target instanceof ScalarArray2D)
+		{
+			processScalar2d((ScalarArray2D<?>) source, (ScalarArray2D<?>) target);
+		}
+		else if (source instanceof ScalarArray3D && target instanceof ScalarArray3D)
+		{
+			processScalar3d((ScalarArray3D<?>) source, (ScalarArray3D<?>) target);
+		}
+		else if (source instanceof ScalarArray && target instanceof ScalarArray)
+		{
+			// most generic implementation, slow...
+			processScalar((ScalarArray<?>) source, (ScalarArray<?>) target);
+		}
+		else
+		{
+			throw new IllegalArgumentException("Can not process array of class " + source.getClass());
+		}
+
+	}
+
+	/**
+	 * Process scalar arrays of any dimension.
+	 */
+	public void processScalar(ScalarArray<?> source, ScalarArray<?> target)
+	{
+		// get array size (for cropping)
+		int nd = source.dimensionality();
+		int[] sizes = source.getSize();
+		
+		// get first two radiuses
+		if (this.radiusList.length < source.dimensionality())
+		{
+			throw new RuntimeException("Requires at least as many radiuses as array dimensionality");
+		}
+		
+		// compute the normalization constant
+		int totalCount = 1;
+		for (int r : this.radiusList)
+		{
+			totalCount *= (2 * r + 1);
+		}
+		
+		double[] values = new double[totalCount];
+		
+		// iterate over positions
+		Cursor inputCursor = source.getCursor();
+		while (inputCursor.hasNext())
+		{
+			// find the position of next element
+			inputCursor.forward();
+			int[] pos = inputCursor.getPosition();
+			
+			// iterate over neighbors
+			Neighborhood nbg = new BoxNeighborhood(pos, radiusList);
+			int count = 0;
+			for (int[] neighPos : nbg)
+			{
+				
+				// clamp neighbor position to array bounds
+				for (int d = 0; d < nd; d++)
+				{
+					neighPos[d] = Math.min(Math.max(neighPos[d], 0), sizes[d]-1);
+				}
+								
+				// get value of "clamped" neighbor
+				values[count++] = source.getValue(neighPos); 
+			}
+			
+			// compute variance value
+			target.setValue(pos, variance(values));
+		}
+	}
+	
+	/**
+	 * Process the specific case of 2D arrays.
+	 */
+	public void processScalar2d(ScalarArray2D<?> source, ScalarArray2D<?> target)
+	{
+		// get size of input array
+		int sizeX = source.getSize(0);
+		int sizeY = source.getSize(1);
+		
+		// get first two radiuses
+		if (this.radiusList.length < 2)
+		{
+			throw new RuntimeException("Can not process 2D array with less than two radiuses.");
+		}
+		int radiusX = this.radiusList[0];
+		int radiusY = this.radiusList[1];
+
+		// compute the normalization constant
+		int totalCount = 1;
+		for (int r : this.radiusList)
+		{
+			totalCount *= (2 * r + 1);
+		}
+		
+		double[] values = new double[totalCount];
+		
+		for(int y = 0; y < sizeY; y++)
+		{
+			this.fireProgressChanged(this, y, sizeY);
+			
+			for(int x = 0; x < sizeX; x++)
+			{
+				// iterate over neighbors of current pixel
+				int count = 0;
+				for (int y2 = y - radiusY; y2 <= y + radiusY; y2++)
+				{
+					int y2r = Math.min(Math.max(y2, 0), sizeY - 1);
+					for (int x2 = x - radiusX; x2 <= x + radiusX; x2++)
+					{
+						int x2r = Math.min(Math.max(x2, 0), sizeX - 1);
+						values[count++] = source.getValue(x2r, y2r); 
+					}
+				}
+				
+				// compute variance value
+				target.setValue(x, y, variance(values));
+			}
+		}
+	}
+
+	/**
+	 * Process the specific case of 3D arrays.
+	 */
+	public void processScalar3d(ScalarArray3D<?> source, ScalarArray3D<?> target)
+	{
+		// get size of input array
+		int sizeX = source.getSize(0);
+		int sizeY = source.getSize(1);
+		int sizeZ = source.getSize(2);
+		
+		// get first three radiuses
+		if (this.radiusList.length < 3)
+		{
+			throw new RuntimeException("Can not process 3D array with less than three radiuses.");
+		}
+		int radiusX = this.radiusList[0];
+		int radiusY = this.radiusList[1];
+		int radiusZ = this.radiusList[2];
+
+		// compute the normalization constant
+		int totalCount = 1;
+		for (int r : this.radiusList)
+		{
+			totalCount *= (2 * r + 1);
+		}
+		
+		double[] values = new double[totalCount];
+		
+		for(int z = 0; z < sizeZ; z++)
+		{
+			this.fireProgressChanged(this, z, sizeZ);
+			
+			for(int y = 0; y < sizeY; y++)
+			{
+				for(int x = 0; x < sizeX; x++)
+				{
+					// iterate over neighbors of current pixel
+					int count = 0;
+					for (int z2 = z - radiusZ; z2 <= z + radiusZ; z2++)
+					{
+						int z2r = Math.min(Math.max(z2, 0), sizeZ - 1);
+						for (int y2 = y - radiusY; y2 <= y + radiusY; y2++)
+						{
+							int y2r = Math.min(Math.max(y2, 0), sizeY - 1);
+							for (int x2 = x - radiusX; x2 <= x + radiusX; x2++)
+							{
+								int x2r = Math.min(Math.max(x2, 0), sizeX - 1);
+								values[count++] = source.getValue(x2r, y2r, z2r); 
+							}
+						}
+					}
+					
+					// compute variance value
+					target.setValue(x, y, z, variance(values));
+				}
+			}
+		}
+	}
+
+	/**
+	 * Computes the variance of the values within the input array.
+	 * 
+	 * @param values an array of values
+	 * @return the variance of the values in the array
+	 */
+	private double variance(double[] values)
+	{
+		int n = values.length;
+		
+		// compute mean
+		double mean = 0;
+		for (double v : values)
+		{
+			mean += v;
+		}
+		mean /= n;
+		
+		// compute variance
+		double var = 0;
+		for (double v : values)
+		{
+			v = v - mean;
+			var += v * v;
+		}
+		var /= n;
+		
+		return var;
+	}
+	
+	/**
+	 * Creates a new array the same size and same type as original.
+	 */
+	public Array<?> createEmptyOutputArray(Array<?> array)
+	{
+		return Float64Array.create(array.getSize());
+	}
+	
+	public boolean canProcess(Array<?> array)
+	{
+		return array instanceof ScalarArray;
+	}
+
+	public boolean canProcess(Array<?> source, Array<?> target)
+	{
+		return source instanceof ScalarArray && target instanceof ScalarArray;
+	}
+}
