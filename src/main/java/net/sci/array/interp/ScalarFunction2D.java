d099a86b81ec14f3e8f47db9d176aead0cb70f4a
dlegland <david.legland@nantes.inra.fr>
2017-07-20 08:28:22 +0200

    update javadoc comments

--- src/main/java/net/sci/array/interp/ScalarFunction2D.java
+++ src/main/java/net/sci/array/interp/ScalarFunction2D.java
@@ -15,11 +15,21 @@ public interface ScalarFunction2D
 {
 	/**
 	 * Evaluates a position given as a couple of coordinates.
+	 * 
+	 * @param x
+	 *            the x-coordinate of the position
+	 * @param y
+	 *            the y-coordinate of the position
+	 * @return the function evaluated at the given position
 	 */
 	public double evaluate(double x, double y);
 	
 	/**
 	 * Evaluates a position given as a point.
+	 * 
+	 * @param p
+	 *            the position for evaluating the function
+	 * @return the function evaluated at the given position
 	 */
 	public default double evaluate(Point2D p)
 	{
--- src/main/java/net/sci/array/interp/ScalarFunction3D.java
+++ src/main/java/net/sci/array/interp/ScalarFunction3D.java
@@ -15,11 +15,23 @@ public interface ScalarFunction3D
 {
 	/**
 	 * Evaluates a position given as a triplet of coordinates.
+	 * 
+	 * @param x
+	 *            the x-coordinate of the position
+	 * @param y
+	 *            the y-coordinate of the position
+	 * @param z
+	 *            the y-coordinate of the position
+	 * @return the function evaluated at the given position
 	 */
 	public double evaluate(double x, double y, double z);
 	
 	/**
 	 * Evaluates a position given as a point.
+	 * 
+	 * @param p
+	 *            the position for evaluating the function
+	 * @return the function evaluated at the given position
 	 */
 	public default double evaluate(Point3D p)
 	{
