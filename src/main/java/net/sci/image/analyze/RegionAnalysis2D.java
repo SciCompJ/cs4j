df9ba300174d156ea0f214d675d7bc23b38a3bd4
dlegland <david.legland@inra.fr>
2017-07-31 10:53:20 +0200

    add computation of inertia ellipses

--- src/main/java/net/sci/image/analyze/RegionAnalysis2D.java
+++ src/main/java/net/sci/image/analyze/RegionAnalysis2D.java
@@ -3,11 +3,14 @@
  */
 package net.sci.image.analyze;
 
+import static java.lang.Math.sqrt;
+
 import java.util.HashMap;
 
 import net.sci.array.data.scalar2d.IntArray2D;
 import net.sci.geom.geom2d.Box2D;
 import net.sci.geom.geom2d.Point2D;
+import net.sci.geom.geom2d.curve.Ellipse2D;
 import net.sci.image.morphology.LabelImages;
 
 /**
@@ -128,4 +131,110 @@ public class RegionAnalysis2D
         
         return boxes;
     }
+    
+    /**
+     * Computes inertia ellipse of each region in input label image.
+     * 
+     * @param labelMap
+     *            the input image containing label of particles
+     * @return an ResultsTable containing for each label, the parameters of the
+     *         inertia ellipsoid, in pixel coordinates
+     */
+    public final static Ellipse2D[] inertiaEllipses(IntArray2D<?> labelMap, int[] labels)
+    {
+        // Check validity of parameters
+        if (labelMap == null)
+            return null;
+
+        // size of image
+        int sizeX = labelMap.getSize(0);
+        int sizeY = labelMap.getSize(1);
+
+        // extract particle labels
+        int nLabels = labels.length;
+
+        // create associative array to know index of each label
+        HashMap<Integer, Integer> labelIndices = LabelImages.mapLabelIndices(labels);
+
+        // allocate memory for result
+        int[] counts = new int[nLabels];
+        double[] cx = new double[nLabels];
+        double[] cy = new double[nLabels];
+        double[] Ixx = new double[nLabels];
+        double[] Iyy = new double[nLabels];
+        double[] Ixy = new double[nLabels];
+
+        // compute the centroid of each region
+        for (int y = 0; y < sizeY; y++) 
+        {
+            for (int x = 0; x < sizeX; x++)
+            {
+                int label = labelMap.getInt(x, y);
+                if (label == 0)
+                    continue;
+
+                int index = labelIndices.get(label);
+                cx[index] += x;
+                cy[index] += y;
+                counts[index]++;
+            }
+        }
+
+        // normalize by the number of pixels in each region
+        for (int i = 0; i < nLabels; i++)
+        {
+            cx[i] = cx[i] / counts[i];
+            cy[i] = cy[i] / counts[i];
+        }
+
+        // compute the centered inertia matrix of each label
+        for (int y = 0; y < sizeY; y++) 
+        {
+            for (int x = 0; x < sizeX; x++)
+            {
+                int label = labelMap.getInt(x, y);
+                if (label == 0)
+                    continue;
+
+                int index = labelIndices.get(label);
+                double x2 = x - cx[index];
+                double y2 = y - cy[index];
+                Ixx[index] += x2 * x2;
+                Ixy[index] += x2 * y2;
+                Iyy[index] += y2 * y2;
+            }
+        }
+
+        // normalize by the number of pixels in each region
+        for (int i = 0; i < nLabels; i++)
+        {
+            Ixx[i] = Ixx[i] / counts[i] + 1. / 12.;
+            Ixy[i] = Ixy[i] / counts[i];
+            Iyy[i] = Iyy[i] / counts[i] + 1. / 12.;
+        }
+
+        // Create data table
+        Ellipse2D[] ellipses = new Ellipse2D[nLabels]; 
+
+        // compute ellipse parameters for each region
+        final double sqrt2 = sqrt(2);
+        for (int i = 0; i < nLabels; i++) 
+        {
+            double xx = Ixx[i];
+            double xy = Ixy[i];
+            double yy = Iyy[i];
+
+            // compute ellipse semi-axes lengths
+            double common = sqrt((xx - yy) * (xx - yy) + 4 * xy * xy);
+            double ra = sqrt2 * sqrt(xx + yy + common);
+            double rb = sqrt2 * sqrt(xx + yy - common);
+
+            // compute ellipse angle and convert into degrees
+            double theta = Math.toDegrees(Math.atan2(2 * xy, xx - yy) / 2);
+            ellipses[i] = new Ellipse2D(cx[i], cy[i], ra, rb, theta);
+        }
+
+        return ellipses;
+    }
+   
 }
