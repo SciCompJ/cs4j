c3c57d2478a815bd2127957c1e293cfcb431fb19
dlegland <david.legland@inra.fr>
2019-07-12 13:17:55 +0200

    add mesh IO package, with OFF read/write classes

---
+++ src/test/java/net/sci/geom/mesh/io/AllTests.java
@@ -0,0 +1,14 @@
+package net.sci.geom.mesh.io;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	// generic classes
+    OffMeshReaderTest.class,
+    OffMeshWriterTest.class,
+	})
+public class AllTests {
+  //nothing
+}
