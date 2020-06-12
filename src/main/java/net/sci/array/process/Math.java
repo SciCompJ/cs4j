db87dd98f4900c2d910d0a993cc0e0dd865f0f9a
dlegland <david.legland@inra.fr>
2020-06-12 16:56:53 +0200

    update Math operations

--- src/main/java/net/sci/array/process/Math.java
+++ src/main/java/net/sci/array/process/Math.java
@@ -17,13 +17,8 @@ import net.sci.array.scalar.ScalarArray;
  */
 public class Math
 {
-
-	/**
-	 * Private constructor to prevent instantiation.
-	 */
-	private Math()
-	{
-	}
+    // =============================================================
+    // Operations involving an array and a scalar value
 
 	/**
 	 * Adds the specified value to each element of the input array, and returns
@@ -37,19 +32,51 @@ public class Math
 	 */
 	public static final ScalarArray<? extends Scalar> add(ScalarArray<? extends Scalar> array, double value)
 	{
-		// create array for result
-		ScalarArray<? extends Scalar> result = array.duplicate();
-		
-		// iterate over elements of each array
-		for (int[] pos : result.positions())
-		{
-		    result.setValue(array.getValue(pos) + value, pos);
-		}
-		
-		// returns the created array
-		return result;
+	    return add(array, value, array.newInstance(array.size()));
 	}
 	
+    /**
+     * Adds the specified value to each element of the input array, and returns
+     * the result in a new array.
+     * 
+     * @param array
+     *            the input array
+     * @param value
+     *            the value to add
+     * @param result
+     *            the output array
+     * @return the result of the element-wise addition
+     */
+    public static final ScalarArray<? extends Scalar> add(
+            ScalarArray<? extends Scalar> array, double value,
+            ScalarArray<? extends Scalar> result)
+    {
+        // iterate over elements of each array
+        for (int[] pos : result.positions())
+        {
+            result.setValue(array.getValue(pos) + value, pos);
+        }
+        
+        // returns the created array
+        return result;
+    }
+    
+    /**
+     * Subtracts the specified value to each element of the input array, and returns
+     * the result in a new array.
+     * 
+     * @param array
+     *            the input array
+     * @param value
+     *            the value to subtract
+     * @return the result of the element-wise subtraction
+     */
+    public static final ScalarArray<? extends Scalar> subtract(
+            ScalarArray<? extends Scalar> array, double value)
+    {
+        return subtract(array, value, array.newInstance(array.size()));
+    }
+    
 	/**
 	 * Subtracts the specified value to each element of the input array, and returns
 	 * the result in a new array.
@@ -60,11 +87,10 @@ public class Math
 	 *            the value to subtract
      * @return the result of the element-wise subtraction
 	 */
-	public static final ScalarArray<? extends Scalar> subtract(ScalarArray<? extends Scalar> array, double value)
+    public static final ScalarArray<? extends Scalar> subtract(
+            ScalarArray<? extends Scalar> array, double value,
+            ScalarArray<? extends Scalar> result)
 	{
-		// create array for result
-		ScalarArray<? extends Scalar> result = array.duplicate();
-		
         // iterate over elements of each array
         for (int[] pos : result.positions())
         {
@@ -87,20 +113,47 @@ public class Math
 	 */
 	public static final ScalarArray<? extends Scalar> multiply(ScalarArray<? extends Scalar> array, double value)
 	{
-		// create array for result
-		ScalarArray<? extends Scalar> result = array.duplicate();
-		
+		return multiply(array, value, array.newInstance(array.size()));
+	}
+	
+    /**
+     * Multiplies the specified value to each element of the input array, and returns
+     * the result in a new array.
+     * 
+     * @param array
+     *            the input array
+     * @param value
+     *            the value to multiply by
+     * @return the result of the element-wise multiplication
+     */
+    public static final ScalarArray<? extends Scalar> multiply(ScalarArray<? extends Scalar> array, double value, ScalarArray<? extends Scalar> result)
+    {
         // iterate over elements of each array
         for (int[] pos : result.positions())
         {
             result.setValue(array.getValue(pos) * value, pos);
         }
-		
-		// returns the created array
-		return result;
-	}
-	
-	/**
+        
+        // returns the created array
+        return result;
+    }
+    
+    /**
+     * Divides the specified value to each element of the input array, and returns
+     * the result in a new array.
+     * 
+     * @param array
+     *            the input array
+     * @param value
+     *            the value to divide by
+     * @return the result of the element-wise division
+     */
+    public static final ScalarArray<? extends Scalar> divide(ScalarArray<? extends Scalar> array, double value)
+    {
+        return divide(array, value, array.newInstance(array.size()));
+    }
+
+    /**
 	 * Divides the specified value to each element of the input array, and returns
 	 * the result in a new array.
 	 * 
@@ -110,11 +163,8 @@ public class Math
 	 *            the value to divide by
 	 * @return the result of the element-wise division
 	 */
-	public static final ScalarArray<? extends Scalar> divide(ScalarArray<? extends Scalar> array, double value)
+	public static final ScalarArray<? extends Scalar> divide(ScalarArray<? extends Scalar> array, double value, ScalarArray<? extends Scalar> result)
 	{
-		// create array for result
-		ScalarArray<? extends Scalar> result = array.duplicate();
-		
         // iterate over elements of each array
         for (int[] pos : result.positions())
         {
@@ -124,6 +174,89 @@ public class Math
 		// returns the created array
 		return result;
 	}
+
+    /**
+     * Adds the specified value to each element of the input array, and returns
+     * the result in a new array.
+     * 
+     * @param array
+     *            the input array
+     * @param value
+     *            the value to compare with
+     * @return the result of the element-wise comparison
+     */
+    public static final ScalarArray<? extends Scalar> min(ScalarArray<? extends Scalar> array, double value)
+    {
+        return add(array, value, array.newInstance(array.size()));
+    }
+    
+    /**
+     * Keeps the minimum of each element compared with the given value.
+     * 
+     * @param array
+     *            the input array
+     * @param value
+     *            the value to compare with
+     * @param result
+     *            the output array
+     * @return the result of the element-wise comparison
+     */
+    public static final ScalarArray<? extends Scalar> min(
+            ScalarArray<? extends Scalar> array, double value,
+            ScalarArray<? extends Scalar> result)
+    {
+        // iterate over elements of each array
+        for (int[] pos : result.positions())
+        {
+            result.setValue(java.lang.Math.min(array.getValue(pos), value), pos);
+        }
+        
+        // returns the created array
+        return result;
+    }
+    
+    /**
+     * Keeps the maximum of each element compared with the given value.
+     * 
+     * @param array
+     *            the input array
+     * @param value
+     *            the value to compare with
+     * @return the result of the element-wise comparison
+     */
+    public static final ScalarArray<? extends Scalar> max(ScalarArray<? extends Scalar> array, double value)
+    {
+        return max(array, value, array.newInstance(array.size()));
+    }
+    
+    /**
+     * Keeps the maximum of each element compared with the given value.
+     * 
+     * @param array
+     *            the input array
+     * @param value
+     *            the value to compare with
+     * @param result
+     *            the output array
+     * @return the result of the element-wise comparison
+     */
+    public static final ScalarArray<? extends Scalar> max(
+            ScalarArray<? extends Scalar> array, double value,
+            ScalarArray<? extends Scalar> result)
+    {
+        // iterate over elements of each array
+        for (int[] pos : result.positions())
+        {
+            result.setValue(java.lang.Math.min(array.getValue(pos), value), pos);
+        }
+        
+        // returns the created array
+        return result;
+    }
+    
+    
+    // =============================================================
+    // Operations involving two arrays
 
     public static final ScalarArray<? extends Scalar> add(
             ScalarArray<? extends Scalar> array1,
@@ -230,6 +363,20 @@ public class Math
         return output;
     }
     
+    /**
+     * Applies a function to each pair of elements read from two arrays, and put
+     * the result in a third array.
+     * 
+     * @param array1
+     *            the first input array
+     * @param array2
+     *            the second input array
+     * @param output
+     *            the output array
+     * @param fun
+     *            the function to apply
+     * @return the reference to the output array.
+     */
     public static final ScalarArray<? extends Scalar> apply(
             ScalarArray<? extends Scalar> array1,
             ScalarArray<? extends Scalar> array2,
@@ -257,5 +404,16 @@ public class Math
         {
             throw new IllegalArgumentException("Arrays must have same size");
         }
+    }
+
+    
+    // =============================================================
+    // Constructor
+
+    /**
+     * Private constructor to prevent instantiation.
+     */
+    private Math()
+    {
     }
 }
