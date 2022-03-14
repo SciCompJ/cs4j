9c71505bb90eb45a6b0386b1440bdb92f2351e67
dlegland <david.legland@inrae.fr>
2022-03-14 09:50:57 +0100

    add array/color/BinaryOverlayRGB8Array.java

---
+++ src/main/java/net/sci/array/color/BinaryOverlayRGB8Array.java
@@ -0,0 +1,155 @@
+/**
+ * 
+ */
+package net.sci.array.color;
+
+import net.sci.array.Array;
+import net.sci.array.Arrays;
+import net.sci.array.binary.BinaryArray;
+import net.sci.array.scalar.UInt8Array;
+
+/**
+ * Creates an RGB8 view that combines original values of a reference array with
+ * a color overlay defined by a binary mask and a specified color.
+ * 
+ * This class keeps references to arrays, and computes the requested RGB colors
+ * on the fly. It requires less memory than creating a new RGB8Array. When
+ * necessary, a deep copy can be obtained via the duplicate() method.
+ * 
+ * @see RGB8Array#duplicate()
+ * 
+ * @author dlegland
+ *
+ */
+public class BinaryOverlayRGB8Array implements RGB8Array
+{
+    /**
+     * The reference array to overlay.
+     */
+    Array<?> baseArray;
+    
+    /**
+     * The binary mask defining the overlay.
+     */
+    BinaryArray binaryArray;
+    
+    /**
+     * The Overlay color. Default is red.
+     */
+    RGB8 overlayColor = RGB8.RED;
+
+    /**
+     * Creates an RGB8 view that combines original values of the reference array
+     * with a color overlay defined by the binary mask and the specified color.
+     * 
+     * @param baseArray
+     *            the reference array to overlay. Can be an instance of
+     *            UInt8Array, BinaryArray, or RGB8Array.
+     * @param binaryArray
+     *            the binary mask defining the overlay. Should be the same size
+     *            as the reference array.
+     * @param overlayColor
+     *            the overlay color.
+     */
+    public BinaryOverlayRGB8Array(Array<?> baseArray, BinaryArray binaryArray, RGB8 overlayColor)
+    {
+        // check input validity
+        if (!Arrays.isSameSize(baseArray, binaryArray))
+        {
+            throw new IllegalArgumentException("Input arrays must have same size"); 
+        }
+        if (!isAllowedType(baseArray))
+        {
+            throw new IllegalArgumentException("Reference array must be an instance of UInt8Array, RGB8Array, or BinaryArray"); 
+        }
+        
+        // copy values
+        this.baseArray = baseArray;
+        this.binaryArray = binaryArray;
+        this.overlayColor = overlayColor;
+    }
+    
+    private boolean isAllowedType(Array<?> array)
+    {
+        if (array instanceof UInt8Array) return true;
+        if (array instanceof RGB8Array) return true;
+        if (array instanceof BinaryArray) return true;
+        return false;
+    }
+
+    @Override
+    public double getValue(int[] pos, int channel)
+    {
+        if (binaryArray.getBoolean(pos))
+        {
+            return overlayColor.getSample(channel);
+        }
+        
+        return getBaseArrayColor(pos).getSample(channel);
+    }
+
+    @Override
+    public void setValue(int[] pos, int channel, double value)
+    {
+        throw new RuntimeException("Can not modify values of a view class");
+    }
+
+    @Override
+    public RGB8 get(int... pos)
+    {
+        if (binaryArray.getBoolean(pos))
+        {
+            return overlayColor;
+        }
+        
+        return getBaseArrayColor(pos);
+    }
+
+    private RGB8 getBaseArrayColor(int... pos)
+    {
+        if (this.baseArray instanceof UInt8Array)
+        {
+            int val = ((UInt8Array) this.baseArray).getInt(pos);
+            return new RGB8(val, val, val);
+        }
+        else if (this.baseArray instanceof RGB8Array)
+        {
+            return ((RGB8Array) this.baseArray).get(pos);
+        }
+        else if (this.baseArray instanceof BinaryArray)
+        {
+            return ((BinaryArray) this.baseArray).getBoolean(pos) ? RGB8.WHITE : RGB8.BLACK;
+        }
+        throw new RuntimeException("Can not process array with class: " + this.baseArray.getClass());
+    }
+
+    @Override
+    public void set(int[] pos, RGB8 value)
+    {
+        throw new RuntimeException("Can not modify values of a view class");
+    }
+    
+    @Override
+    public int dimensionality()
+    {
+        return baseArray.dimensionality();
+    }
+
+    @Override
+    public int[] size()
+    {
+        return baseArray.size();
+    }
+
+    @Override
+    public int size(int dim)
+    {
+        return baseArray.size(dim);
+    }
+
+    @Override
+    public PositionIterator positionIterator()
+    {
+        return baseArray.positionIterator();
+    }
+}
---
+++ src/test/java/net/sci/array/color/BinaryOverlayRGB8ArrayTest.java
@@ -0,0 +1,309 @@
+/**
+ * 
+ */
+package net.sci.array.color;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import net.sci.array.binary.BinaryArray2D;
+import net.sci.array.binary.BinaryArray3D;
+import net.sci.array.scalar.UInt8Array2D;
+import net.sci.array.scalar.UInt8Array3D;
+
+/**
+ * @author dlegland
+ *
+ */
+public class BinaryOverlayRGB8ArrayTest
+{
+    /**
+     * Test method for {@link net.sci.array.color.BinaryOverlayRGB8Array#get(int[])}.
+     */
+    @Test
+    public final void testGet_2d()
+    {
+        BinaryOverlayRGB8Array array = createSampleArray_2d();
+        
+        // check image four corners
+        assertEquals(new RGB8(  0,   0,   0), array.get(0, 0));
+        assertEquals(new RGB8(  9,   9,   9), array.get(9, 0));
+        assertEquals(new RGB8( 70,  70,  70), array.get(0, 7));
+        assertEquals(new RGB8( 79,  79,  79), array.get(9, 7));
+        // check four corners or the region in binary image
+        assertEquals(RGB8.RED, array.get(2, 2));
+        assertEquals(RGB8.RED, array.get(7, 2));
+        assertEquals(RGB8.RED, array.get(2, 5));
+        assertEquals(RGB8.RED, array.get(7, 5));
+    }
+
+    /**
+     * Test method for {@link net.sci.array.color.BinaryOverlayRGB8Array#getValue(int[], int)}.
+     */
+    @Test
+    public final void testGetSample_2d()
+    {
+        BinaryOverlayRGB8Array array = createSampleArray_2d();
+        
+        // check image four corners
+        assertEquals(0, array.getSample(new int[] {0, 0}, 0));
+        assertEquals(0, array.getSample(new int[] {0, 0}, 1));
+        assertEquals(0, array.getSample(new int[] {0, 0}, 2));
+        assertEquals(9, array.getSample(new int[] {9, 0}, 0));
+        assertEquals(9, array.getSample(new int[] {9, 0}, 1));
+        assertEquals(9, array.getSample(new int[] {9, 0}, 2));
+        assertEquals(70, array.getSample(new int[] {0, 7}, 0));
+        assertEquals(70, array.getSample(new int[] {0, 7}, 1));
+        assertEquals(70, array.getSample(new int[] {0, 7}, 2));
+        assertEquals(79, array.getSample(new int[] {9, 7}, 0));
+        assertEquals(79, array.getSample(new int[] {9, 7}, 1));
+        assertEquals(79, array.getSample(new int[] {9, 7}, 2));
+        
+        // check four corners or the region in binary image
+        assertEquals(255, array.getSample(new int[] {2, 2}, 0));
+        assertEquals(  0, array.getSample(new int[] {2, 2}, 1));
+        assertEquals(  0, array.getSample(new int[] {2, 2}, 2));
+        assertEquals(255, array.getSample(new int[] {7, 2}, 0));
+        assertEquals(  0, array.getSample(new int[] {7, 2}, 1));
+        assertEquals(  0, array.getSample(new int[] {7, 2}, 2));
+        assertEquals(255, array.getSample(new int[] {2, 5}, 0));
+        assertEquals(  0, array.getSample(new int[] {2, 5}, 1));
+        assertEquals(  0, array.getSample(new int[] {2, 5}, 2));
+        assertEquals(255, array.getSample(new int[] {7, 5}, 0));
+        assertEquals(  0, array.getSample(new int[] {7, 5}, 1));
+        assertEquals(  0, array.getSample(new int[] {7, 5}, 2));
+    }
+
+    /**
+     * Test method for {@link net.sci.array.color.BinaryOverlayRGB8Array#getValue(int[], int)}.
+     */
+    @Test
+    public final void testGetValue_2d()
+    {
+        BinaryOverlayRGB8Array array = createSampleArray_2d();
+        
+        // check image four corners
+        assertEquals(0, array.getValue(new int[] {0, 0}, 0), 0.1);
+        assertEquals(0, array.getValue(new int[] {0, 0}, 1), 0.1);
+        assertEquals(0, array.getValue(new int[] {0, 0}, 2), 0.1);
+        assertEquals(9, array.getValue(new int[] {9, 0}, 0), 0.1);
+        assertEquals(9, array.getValue(new int[] {9, 0}, 1), 0.1);
+        assertEquals(9, array.getValue(new int[] {9, 0}, 2), 0.1);
+        assertEquals(70, array.getValue(new int[] {0, 7}, 0), 0.1);
+        assertEquals(70, array.getValue(new int[] {0, 7}, 1), 0.1);
+        assertEquals(70, array.getValue(new int[] {0, 7}, 2), 0.1);
+        assertEquals(79, array.getValue(new int[] {9, 7}, 0), 0.1);
+        assertEquals(79, array.getValue(new int[] {9, 7}, 1), 0.1);
+        assertEquals(79, array.getValue(new int[] {9, 7}, 2), 0.1);
+        
+        // check four corners or the region in binary image
+        assertEquals(255, array.getValue(new int[] {2, 2}, 0), 0.1);
+        assertEquals(  0, array.getValue(new int[] {2, 2}, 1), 0.1);
+        assertEquals(  0, array.getValue(new int[] {2, 2}, 2), 0.1);
+        assertEquals(255, array.getValue(new int[] {7, 2}, 0), 0.1);
+        assertEquals(  0, array.getValue(new int[] {7, 2}, 1), 0.1);
+        assertEquals(  0, array.getValue(new int[] {7, 2}, 2), 0.1);
+        assertEquals(255, array.getValue(new int[] {2, 5}, 0), 0.1);
+        assertEquals(  0, array.getValue(new int[] {2, 5}, 1), 0.1);
+        assertEquals(  0, array.getValue(new int[] {2, 5}, 2), 0.1);
+        assertEquals(255, array.getValue(new int[] {7, 5}, 0), 0.1);
+        assertEquals(  0, array.getValue(new int[] {7, 5}, 1), 0.1);
+        assertEquals(  0, array.getValue(new int[] {7, 5}, 2), 0.1);
+    }
+
+    /**
+     * Test method for {@link net.sci.array.color.BinaryOverlayRGB8Array#iterator()}.
+     */
+    @Test
+    public final void testIterator_2d()
+    {
+        BinaryOverlayRGB8Array array = createSampleArray_2d();
+        int count = 0;
+        for (@SuppressWarnings("unused") RGB8 rgb : array)
+        {
+            count++;
+        }
+        assertEquals(80, count);
+    }
+    
+    /**
+     * Creates a RGB8 view, from a UInt8Array with size(10,8) and a binary mask
+     * with same size.
+     *  
+     * @return a sample 10-by-8 RGB8 overlay view
+     */
+    private BinaryOverlayRGB8Array createSampleArray_2d()
+    {
+        UInt8Array2D baseArray = UInt8Array2D.create(10, 8);
+        baseArray.fillInts((x,y) -> y * 10 + x);
+        BinaryArray2D binaryArray = BinaryArray2D.create(10, 8);
+        binaryArray.fillBooleans((x,y) -> x >= 2 && x <= 7 && y >= 2 && y <= 5);
+        
+        BinaryOverlayRGB8Array res = new BinaryOverlayRGB8Array(baseArray, binaryArray, RGB8.RED);
+        return res;
+    }
+    
+    /**
+     * Test method for {@link net.sci.array.color.BinaryOverlayRGB8Array#get(int[])}.
+     */
+    @Test
+    public final void testGet_3d()
+    {
+        BinaryOverlayRGB8Array array = createSampleArray_3d();
+        
+        // check image eight corners
+        assertEquals(new RGB8(  0,   0,   0), array.get(0, 0, 0));
+        assertEquals(new RGB8(  4,   4,   4), array.get(4, 0, 0));
+        assertEquals(new RGB8( 30,  30,  30), array.get(0, 3, 0));
+        assertEquals(new RGB8( 34,  34,  34), array.get(4, 3, 0));
+        assertEquals(new RGB8(200, 200, 200), array.get(0, 0, 2));
+        assertEquals(new RGB8(204, 204, 204), array.get(4, 0, 2));
+        assertEquals(new RGB8(230, 230, 230), array.get(0, 3, 2));
+        assertEquals(new RGB8(234, 234, 234), array.get(4, 3, 2));
+        // check four corners of the region in binary image
+        assertEquals(RGB8.RED, array.get(1, 1, 1));
+        assertEquals(RGB8.RED, array.get(3, 1, 1));
+        assertEquals(RGB8.RED, array.get(1, 2, 1));
+        assertEquals(RGB8.RED, array.get(3, 2, 1));
+    }
+
+    /**
+     * Test method for {@link net.sci.array.color.BinaryOverlayRGB8Array#getValue(int[], int)}.
+     */
+    @Test
+    public final void testGetSample_3d()
+    {
+        BinaryOverlayRGB8Array array = createSampleArray_3d();
+        
+        // check image eight corners
+        assertEquals(  0, array.getSample(new int[] {0, 0, 0}, 0));
+        assertEquals(  0, array.getSample(new int[] {0, 0, 0}, 1));
+        assertEquals(  0, array.getSample(new int[] {0, 0, 0}, 2));
+        assertEquals(  4, array.getSample(new int[] {4, 0, 0}, 0));
+        assertEquals(  4, array.getSample(new int[] {4, 0, 0}, 1));
+        assertEquals(  4, array.getSample(new int[] {4, 0, 0}, 2));
+        assertEquals( 30, array.getSample(new int[] {0, 3, 0}, 0));
+        assertEquals( 30, array.getSample(new int[] {0, 3, 0}, 1));
+        assertEquals( 30, array.getSample(new int[] {0, 3, 0}, 2));
+        assertEquals( 34, array.getSample(new int[] {4, 3, 0}, 0));
+        assertEquals( 34, array.getSample(new int[] {4, 3, 0}, 1));
+        assertEquals( 34, array.getSample(new int[] {4, 3, 0}, 2));
+        assertEquals(200, array.getSample(new int[] {0, 0, 2}, 0));
+        assertEquals(200, array.getSample(new int[] {0, 0, 2}, 1));
+        assertEquals(200, array.getSample(new int[] {0, 0, 2}, 2));
+        assertEquals(204, array.getSample(new int[] {4, 0, 2}, 0));
+        assertEquals(204, array.getSample(new int[] {4, 0, 2}, 1));
+        assertEquals(204, array.getSample(new int[] {4, 0, 2}, 2));
+        assertEquals(230, array.getSample(new int[] {0, 3, 2}, 0));
+        assertEquals(230, array.getSample(new int[] {0, 3, 2}, 1));
+        assertEquals(230, array.getSample(new int[] {0, 3, 2}, 2));
+        assertEquals(234, array.getSample(new int[] {4, 3, 2}, 0));
+        assertEquals(234, array.getSample(new int[] {4, 3, 2}, 1));
+        assertEquals(234, array.getSample(new int[] {4, 3, 2}, 2));
+        
+        // check four corners or the region in binary image
+        assertEquals(255, array.getSample(new int[] {1, 1, 1}, 0));
+        assertEquals(  0, array.getSample(new int[] {1, 1, 1}, 1));
+        assertEquals(  0, array.getSample(new int[] {1, 1, 1}, 2));
+        assertEquals(255, array.getSample(new int[] {3, 1, 1}, 0));
+        assertEquals(  0, array.getSample(new int[] {3, 1, 1}, 1));
+        assertEquals(  0, array.getSample(new int[] {3, 1, 1}, 2));
+        assertEquals(255, array.getSample(new int[] {1, 2, 1}, 0));
+        assertEquals(  0, array.getSample(new int[] {1, 2, 1}, 1));
+        assertEquals(  0, array.getSample(new int[] {1, 2, 1}, 2));
+        assertEquals(255, array.getSample(new int[] {3, 2, 1}, 0));
+        assertEquals(  0, array.getSample(new int[] {3, 2, 1}, 1));
+        assertEquals(  0, array.getSample(new int[] {3, 2, 1}, 2));
+    }
+
+    /**
+     * Test method for {@link net.sci.array.color.BinaryOverlayRGB8Array#getValue(int[], int)}.
+     */
+    @Test
+    public final void testGetValue_3d()
+    {
+        BinaryOverlayRGB8Array array = createSampleArray_3d();
+        
+        // check image eight corners
+        assertEquals(  0, array.getValue(new int[] {0, 0, 0}, 0), 0.1);
+        assertEquals(  0, array.getValue(new int[] {0, 0, 0}, 1), 0.1);
+        assertEquals(  0, array.getValue(new int[] {0, 0, 0}, 2), 0.1);
+        assertEquals(  4, array.getValue(new int[] {4, 0, 0}, 0), 0.1);
+        assertEquals(  4, array.getValue(new int[] {4, 0, 0}, 1), 0.1);
+        assertEquals(  4, array.getValue(new int[] {4, 0, 0}, 2), 0.1);
+        assertEquals( 30, array.getValue(new int[] {0, 3, 0}, 0), 0.1);
+        assertEquals( 30, array.getValue(new int[] {0, 3, 0}, 1), 0.1);
+        assertEquals( 30, array.getValue(new int[] {0, 3, 0}, 2), 0.1);
+        assertEquals( 34, array.getValue(new int[] {4, 3, 0}, 0), 0.1);
+        assertEquals( 34, array.getValue(new int[] {4, 3, 0}, 1), 0.1);
+        assertEquals( 34, array.getValue(new int[] {4, 3, 0}, 2), 0.1);
+        assertEquals(200, array.getValue(new int[] {0, 0, 2}, 0), 0.1);
+        assertEquals(200, array.getValue(new int[] {0, 0, 2}, 1), 0.1);
+        assertEquals(200, array.getValue(new int[] {0, 0, 2}, 2), 0.1);
+        assertEquals(204, array.getValue(new int[] {4, 0, 2}, 0), 0.1);
+        assertEquals(204, array.getValue(new int[] {4, 0, 2}, 1), 0.1);
+        assertEquals(204, array.getValue(new int[] {4, 0, 2}, 2), 0.1);
+        assertEquals(230, array.getValue(new int[] {0, 3, 2}, 0), 0.1);
+        assertEquals(230, array.getValue(new int[] {0, 3, 2}, 1), 0.1);
+        assertEquals(230, array.getValue(new int[] {0, 3, 2}, 2), 0.1);
+        assertEquals(234, array.getValue(new int[] {4, 3, 2}, 0), 0.1);
+        assertEquals(234, array.getValue(new int[] {4, 3, 2}, 1), 0.1);
+        assertEquals(234, array.getValue(new int[] {4, 3, 2}, 2), 0.1);
+        
+        // check four corners or the region in binary image
+        assertEquals(255, array.getValue(new int[] {1, 1, 1}, 0), 0.1);
+        assertEquals(  0, array.getValue(new int[] {1, 1, 1}, 1), 0.1);
+        assertEquals(  0, array.getValue(new int[] {1, 1, 1}, 2), 0.1);
+        assertEquals(255, array.getValue(new int[] {3, 1, 1}, 0), 0.1);
+        assertEquals(  0, array.getValue(new int[] {3, 1, 1}, 1), 0.1);
+        assertEquals(  0, array.getValue(new int[] {3, 1, 1}, 2), 0.1);
+        assertEquals(255, array.getValue(new int[] {1, 2, 1}, 0), 0.1);
+        assertEquals(  0, array.getValue(new int[] {1, 2, 1}, 1), 0.1);
+        assertEquals(  0, array.getValue(new int[] {1, 2, 1}, 2), 0.1);
+        assertEquals(255, array.getValue(new int[] {3, 2, 1}, 0), 0.1);
+        assertEquals(  0, array.getValue(new int[] {3, 2, 1}, 1), 0.1);
+        assertEquals(  0, array.getValue(new int[] {3, 2, 1}, 2), 0.1);
+    }
+
+    /**
+     * Test method for {@link net.sci.array.color.BinaryOverlayRGB8Array#iterator()}.
+     */
+    @Test
+    public final void testIterator_3d()
+    {
+        BinaryOverlayRGB8Array array = createSampleArray_3d();
+        int count = 0;
+        int redCount = 0;
+        
+        for (RGB8 rgb : array)
+        {
+            count++;
+            if (rgb == RGB8.RED)
+            {
+                redCount++;
+            }
+        }
+        
+        assertEquals(60, count);
+        assertEquals(6, redCount);
+    }
+    
+    /**
+     * Creates a RGB8 view, from a UInt8Array with size(5,4,3) and a binary mask
+     * with same size.
+     *  
+     * @return a sample 5--by-4-by-3 RGB8 overlay view
+     */
+    private BinaryOverlayRGB8Array createSampleArray_3d()
+    {
+        UInt8Array3D baseArray = UInt8Array3D.create(5, 4, 3);
+        baseArray.fillInts((x,y,z) -> z * 100 + y * 10 + x);
+        BinaryArray3D binaryArray = BinaryArray3D.create(5, 4, 3);
+        binaryArray.fillBooleans((x,y,z) -> x >= 1 && x <= 3 && y >= 1 && y <= 2 && z == 1);
+        
+        BinaryOverlayRGB8Array res = new BinaryOverlayRGB8Array(baseArray, binaryArray, RGB8.RED);
+        return res;
+    }
+
+}
