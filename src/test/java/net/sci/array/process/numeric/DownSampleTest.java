6887eeb9c972b040f809631fd171b2abbccb011e
dlegland <david.legland@inrae.fr>
2022-10-24 00:09:45 +0200

    add DownSample.java

---
+++ src/test/java/net/sci/array/process/numeric/DownSampleTest.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package net.sci.array.process.numeric;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import net.sci.array.scalar.ScalarArray2D;
+import net.sci.array.scalar.UInt8Array2D;
+
+/**
+ * @author dlegland
+ *
+ */
+public class DownSampleTest
+{
+
+    /**
+     * Test method for {@link net.sci.array.process.numeric.DownSample#processScalar(net.sci.array.scalar.ScalarArray)}.
+     */
+    @Test
+    public final void testProcessScalar_checkSize()
+    {
+        UInt8Array2D array = UInt8Array2D.create(10, 8);
+        
+        DownSample op = new DownSample(2);
+        
+        ScalarArray2D<?> res = ScalarArray2D.wrapScalar2d(op.processScalar(array));
+
+        assertEquals(5, res.size(0));
+        assertEquals(4, res.size(1));
+    }
+
+}
