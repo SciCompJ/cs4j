3a551fc16c8430aa93f3364234c679354c13a5b0
dlegland <david.legland@inrae.fr>
2023-01-05 11:55:56 +0100

    add KeepValueRange operator

---
+++ src/test/java/net/sci/array/process/numeric/KeepValueRangeTest.java
@@ -0,0 +1,71 @@
+/**
+ * 
+ */
+package net.sci.array.process.numeric;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import net.sci.array.scalar.ScalarArray;
+import net.sci.array.scalar.UInt8Array2D;
+import net.sci.array.scalar.UInt8Array3D;
+
+/**
+ * @author dlegland
+ *
+ */
+public class KeepValueRangeTest
+{
+
+    /**
+     * Test method for {@link net.sci.array.process.numeric.KeepValueRange#processScalar(net.sci.array.scalar.ScalarArray, net.sci.array.scalar.ScalarArray)}.
+     */
+    @Test
+    public final void testProcessScalar_2d()
+    {
+        UInt8Array2D array = UInt8Array2D.create(20, 6);
+        array.fillValues((x,y) -> x * 10.0);
+        
+        KeepValueRange algo = new KeepValueRange(50, 150); 
+        ScalarArray<?> res = algo.processScalar(array);
+        
+        assertEquals(2, res.dimensionality());
+        assertEquals(array.size(0), res.size(0));
+        assertEquals(array.size(1), res.size(1));
+        
+        assertEquals(  0.0, res.getValue( 0, 0), 0.0);
+        assertEquals(  0.0, res.getValue( 4, 0), 0.0);
+        assertEquals( 50.0, res.getValue( 5, 0), 0.0);
+        assertEquals(150.0, res.getValue(15, 0), 0.0);
+        assertEquals(  0.0, res.getValue(16, 0), 0.0);
+        assertEquals(  0.0, res.getValue(19, 0), 0.0);
+    }
+
+
+    /**
+     * Test method for {@link net.sci.array.process.numeric.KeepValueRange#processScalar(net.sci.array.scalar.ScalarArray, net.sci.array.scalar.ScalarArray)}.
+     */
+    @Test
+    public final void testProcessScalar_3d()
+    {
+        UInt8Array3D array = UInt8Array3D.create(20, 6, 4);
+        array.fillValues((x,y,z) -> x * 10.0);
+        
+        KeepValueRange algo = new KeepValueRange(50, 150); 
+        ScalarArray<?> res = algo.processScalar(array);
+        
+        assertEquals(3, res.dimensionality());
+        assertEquals(array.size(0), res.size(0));
+        assertEquals(array.size(1), res.size(1));
+        assertEquals(array.size(2), res.size(2));
+        
+        assertEquals(  0.0, res.getValue( 0, 0, 0), 0.0);
+        assertEquals(  0.0, res.getValue( 4, 0, 0), 0.0);
+        assertEquals( 50.0, res.getValue( 5, 0, 0), 0.0);
+        assertEquals(150.0, res.getValue(15, 0, 0), 0.0);
+        assertEquals(  0.0, res.getValue(16, 0, 0), 0.0);
+        assertEquals(  0.0, res.getValue(19, 0, 0), 0.0);
+    }
+
+}
