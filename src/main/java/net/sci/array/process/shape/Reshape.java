6f535fd4674b7ff5c4dbd2e2094dee09a7530fcb
dlegland <david.legland@inrae.fr>
2024-02-07 15:09:12 +0100

    array/process/shape/Reshape: add possibility to return a view

--- src/main/java/net/sci/array/process/shape/Reshape.java
+++ src/main/java/net/sci/array/process/shape/Reshape.java
@@ -18,50 +18,81 @@ import net.sci.util.MathUtils;
  */
 public class Reshape extends AlgoStub implements ArrayOperator
 {
-	int[] newDims;
-	
-	/**
-	 * Creates a reshape operator.
-	 * 
-	 *  @param newDims the dimensions of the array after reshaping
-	 */
-	public Reshape(int[] newDims)
-	{
-	    this.newDims = newDims;
-	}
+    int[] newDims;
 
-	/* (non-Javadoc)
-	 * @see net.sci.array.ArrayOperator#process(net.sci.array.Array)
-	 */
-	@Override
-	public <T> Array<T> process(Array<T> array)
-	{
-	    // compute element number of input array
-	    long count = array.elementCount();
-	    
+    /**
+     * Creates a reshape operator.
+     * 
+     * @param newDims
+     *            the dimensions of the array after reshaping
+     */
+    public Reshape(int[] newDims)
+    {
+        this.newDims = newDims;
+    }
+    
+    public <T> Array<T> view(Array<T> array)
+    {
+        long[] cumDims = MathUtils.cumProdLong(array.size());
+        long[] targetCumDims = MathUtils.cumProdLong(newDims);
+        
+        return array.reshapeView(newDims, pos -> 
+        {
+            // first convert view coordinates into linear index 
+            long index = pos[0];
+            for (int d = 1; d < pos.length; d++)
+            {
+                index += targetCumDims[d - 1] * pos[d];
+            }
+            
+            // convert linear index into coordinates into original array 
+            int[] res = new int[array.dimensionality()];
+            for (int d = res.length - 1; d > 0; d--)
+            {
+                long numel = cumDims[d - 1];
+                res[d] = (int) (index / numel);
+                index -= (res[d] * numel);
+            }
+            res[0] = (int) index;
+            
+            return res;
+        });
+    }
+    
+    /*
+     * (non-Javadoc)
+     * 
+     * @see net.sci.array.ArrayOperator#process(net.sci.array.Array)
+     */
+    @Override
+    public <T> Array<T> process(Array<T> array)
+    {
+        // compute element number of input array
+        long count = array.elementCount();
+
         // compute element number of output array
-	    long count2 = MathUtils.prod(newDims);
-	    
-	    // check element numbers are the same
-	    if (count != count2)
-	    {
-	        throw new IllegalArgumentException("Input array should have same number of elements as product of dimensions");
-	    }
-	    
-	    // allocate memory
-	    Array<T> result = array.newInstance(newDims);
-	    
-	    // copy elements of input array to result
+        long count2 = MathUtils.prod(newDims);
+
+        // check element numbers are the same
+        if (count != count2)
+        {
+            throw new IllegalArgumentException("Input array should have same number of elements as product of dimensions");
+        }
+
+        // allocate memory
+        Array<T> result = array.newInstance(newDims);
+
+        // copy elements of input array to result
         Array.Iterator<T> iter1 = array.iterator();
         Array.Iterator<T> iter2 = result.iterator();
-        while(iter1.hasNext())
+        while (iter1.hasNext())
         {
             iter2.setNext(iter1.next());
         }
         
         return result;
-	}
-
+    }
+    
     /**
      * Checks if the operator can be applied to the specified array, by
      * comparing the number of elements before and after reshape.
@@ -71,9 +102,9 @@ public class Reshape extends AlgoStub implements ArrayOperator
      * @return true if array as same number of elements as the number of
      *         dimensions stored in this operator.
      */
-	@Override
-	public boolean canProcess(Array<?> array)
-	{
+    @Override
+    public boolean canProcess(Array<?> array)
+    {
         // compute element number of input array
         long prodDims = array.elementCount();
         
@@ -82,5 +113,5 @@ public class Reshape extends AlgoStub implements ArrayOperator
         
         // check element numbers are the same
         return prodDims == prodDims2;
-	}
+    }
 }
---
+++ src/test/java/net/sci/array/process/shape/ReshapeTest.java
@@ -0,0 +1,109 @@
+/**
+ * 
+ */
+package net.sci.array.process.shape;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import net.sci.array.scalar.UInt8Array;
+import net.sci.array.scalar.UInt8Array2D;
+import net.sci.array.scalar.UInt8Array3D;
+
+/**
+ * 
+ */
+public class ReshapeTest
+{
+    /**
+     * Test method for {@link net.sci.array.process.shape.Reshape#process(net.sci.array.Array)}.
+     */
+    @Test
+    public final void test_process_2d_to_2d()
+    {
+        UInt8Array2D array = UInt8Array2D.create(6, 4);
+        array.fillInts((x,y)-> 6 * y + x);
+        
+        Reshape op = new Reshape(new int[] {4, 6});
+        UInt8Array2D res = UInt8Array2D.wrap(UInt8Array.wrap(op.process(array)));
+        
+        assertEquals(res.size(0), 4);
+        assertEquals(res.size(1), 6);
+        assertEquals(res.getInt(0,0), 0);
+        assertEquals(res.getInt(3,0), 3);
+        assertEquals(res.getInt(0,5), 20);
+        assertEquals(res.getInt(3,5), 23);
+    }
+
+    /**
+     * Test method for {@link net.sci.array.process.shape.Reshape#process(net.sci.array.Array)}.
+     */
+    @Test
+    public final void test_process_2d_to_3d()
+    {
+        UInt8Array2D array = UInt8Array2D.create(6, 4);
+        array.fillInts((x,y)-> 6 * y + x);
+        
+        Reshape op = new Reshape(new int[] {4, 3, 2});
+        UInt8Array3D res = UInt8Array3D.wrap(UInt8Array.wrap(op.process(array)));
+        
+        assertEquals(res.size(0), 4);
+        assertEquals(res.size(1), 3);
+        assertEquals(res.size(2), 2);
+        assertEquals(res.getInt(0,0,0),  0);
+        assertEquals(res.getInt(3,0,0),  3);
+        assertEquals(res.getInt(0,2,0),  8);
+        assertEquals(res.getInt(3,2,0), 11);
+        assertEquals(res.getInt(0,0,1), 12);
+        assertEquals(res.getInt(3,0,1), 15);
+        assertEquals(res.getInt(0,2,1), 20);
+        assertEquals(res.getInt(3,2,1), 23);
+    }
+
+    /**
+     * Test method for {@link net.sci.array.process.shape.Reshape#view(net.sci.array.Array)}.
+     */
+    @Test
+    public final void test_view_2d_to_2d()
+    {
+        UInt8Array2D array = UInt8Array2D.create(6, 4);
+        array.fillInts((x,y)-> 6 * y + x);
+        
+        Reshape op = new Reshape(new int[] {4, 6});
+        UInt8Array2D res = UInt8Array2D.wrap(UInt8Array.wrap(op.view(array)));
+        
+        assertEquals(res.size(0), 4);
+        assertEquals(res.size(1), 6);
+        assertEquals(res.getInt(0,0), 0);
+        assertEquals(res.getInt(3,0), 3);
+        assertEquals(res.getInt(0,5), 20);
+        assertEquals(res.getInt(3,5), 23);
+    }
+
+    /**
+     * Test method for {@link net.sci.array.process.shape.Reshape#view(net.sci.array.Array)}.
+     */
+    @Test
+    public final void test_view_2d_to_3d()
+    {
+        UInt8Array2D array = UInt8Array2D.create(6, 4);
+        array.fillInts((x,y)-> 6 * y + x);
+        
+        Reshape op = new Reshape(new int[] {4, 3, 2});
+        UInt8Array3D res = UInt8Array3D.wrap(UInt8Array.wrap(op.view(array)));
+        
+        assertEquals(res.size(0), 4);
+        assertEquals(res.size(1), 3);
+        assertEquals(res.size(2), 2);
+        assertEquals(res.getInt(0,0,0),  0);
+        assertEquals(res.getInt(3,0,0),  3);
+        assertEquals(res.getInt(0,2,0),  8);
+        assertEquals(res.getInt(3,2,0), 11);
+        assertEquals(res.getInt(0,0,1), 12);
+        assertEquals(res.getInt(3,0,1), 15);
+        assertEquals(res.getInt(0,2,1), 20);
+        assertEquals(res.getInt(3,2,1), 23);
+    }
+
+}
