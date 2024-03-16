84db4dc133d81cef0440c5f8eb3ae3c89ee1adba
dlegland <david.legland@inrae.fr>
2024-03-16 16:26:32 +0100

    array/process/numeric: add AddValue class

---
+++ src/main/java/net/sci/array/process/numeric/AddValue.java
@@ -0,0 +1,93 @@
+/**
+ * 
+ */
+package net.sci.array.process.numeric;
+
+import net.sci.algo.AlgoStub;
+import net.sci.array.ArrayWrapperStub;
+import net.sci.array.process.ScalarArrayOperator;
+import net.sci.array.scalar.Scalar;
+import net.sci.array.scalar.ScalarArray;
+
+/**
+ * Adds a scalar value to an array containing Scalar value.
+ * 
+ * @author dlegland
+ *
+ */
+public class AddValue extends AlgoStub implements ScalarArrayOperator
+{
+    /** the value to add to each element of the original array.*/
+    double value;
+    
+    public AddValue(double value)
+    {
+        this.value = value;
+    }
+    
+    public <S extends Scalar<S>> ScalarArray<?> createView(ScalarArray<S> array)
+    {
+        return new View<S>(array);
+    }
+    
+    @Override
+    public ScalarArray<?> processScalar(ScalarArray<? extends Scalar<?>> array)
+    {
+        ScalarArray<?> res = array.newInstance(array.size());
+        res.fillValues(pos -> array.getValue(pos) + value);
+        return res;
+    }
+    
+    public class View <S extends Scalar<S>> extends ArrayWrapperStub<S> implements ScalarArray<S>
+    {
+        ScalarArray<S> array;
+        
+        public View(ScalarArray<S> array)
+        {
+            super(array);
+            this.array = array;
+        }
+        
+        @Override
+        public double getValue(int[] pos)
+        {
+            return array.getValue(pos) + value;
+        }
+
+        @Override
+        public void setValue(int[] pos, double value)
+        {
+            throw new RuntimeException("Can not modify value of a view");
+        }
+
+        @Override
+        public S get(int[] pos)
+        {
+            return array.createElement(array.getValue(pos) + value);
+        }
+
+        @Override
+        public void set(int[] pos, S value)
+        {
+            throw new RuntimeException("Can not modify value of a view");
+        }
+
+        @Override
+        public Class<S> elementClass()
+        {
+            return array.elementClass();
+        }
+
+        @Override
+        public ScalarArray<S> newInstance(int... dims)
+        {
+            return array.newInstance(dims);
+        }
+
+        @Override
+        public ScalarArray.Factory<S> factory()
+        {
+            return array.factory();
+        }
+    }
+}
---
+++ src/test/java/net/sci/array/process/numeric/AddValueTest.java
@@ -0,0 +1,63 @@
+/**
+ * 
+ */
+package net.sci.array.process.numeric;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import net.sci.array.scalar.ScalarArray2D;
+import net.sci.array.scalar.UInt8Array2D;
+
+/**
+ * @author dlegland
+ *
+ */
+public class AddValueTest
+{
+    /**
+     * Test method for {@link net.sci.array.process.numeric.AddValue#processScalar(net.sci.array.scalar.ScalarArray)}.
+     */
+    @Test
+    public final void testProcess_UInt8Array()
+    {
+        UInt8Array2D array = UInt8Array2D.create(6, 4);
+        array.fillInts((x, y) -> y * 10 + x);
+        
+        ScalarArray2D<?> res = ScalarArray2D.wrapScalar2d(new AddValue(10.0).processScalar(array));
+        
+        assertEquals(array.size(0), res.size(0));
+        assertEquals(array.size(1), res.size(1));
+        
+        assertEquals(10.0, res.getValue(0,0), 0.01);
+        assertEquals(15.0, res.getValue(5,0), 0.01);
+        assertEquals(40.0, res.getValue(0,3), 0.01);
+        assertEquals(45.0, res.getValue(5,3), 0.01);
+    }
+
+    /**
+     * Test method for {@link net.sci.array.process.numeric.AddValue#processScalar(net.sci.array.scalar.ScalarArray)}.
+     */
+    @Test
+    public final void testView_UInt8Array()
+    {
+        UInt8Array2D array = UInt8Array2D.create(6, 4);
+        array.fillInts((x, y) -> y * 10 + x);
+        
+        ScalarArray2D<?> res = ScalarArray2D.wrapScalar2d(new AddValue(10.0).createView(array));
+        
+        assertEquals(array.size(0), res.size(0));
+        assertEquals(array.size(1), res.size(1));
+        
+        assertEquals(10.0, res.getValue(0,0), 0.01);
+        assertEquals(15.0, res.getValue(5,0), 0.01);
+        assertEquals(40.0, res.getValue(0,3), 0.01);
+        assertEquals(45.0, res.getValue(5,3), 0.01);
+        
+        // updates reference array, and check change on the view
+        array.setValue(2, 1, 50.0);
+        assertEquals(60.0, res.getValue(2, 1), 0.01);
+    }
+
+}
