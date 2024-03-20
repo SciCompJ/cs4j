981c7a923fbbcd5b724e2c7ffeca843e5f76e268
dlegland <david.legland@inrae.fr>
2024-03-20 17:33:11 +0100

    image/process: computation of Sobel gradient(s) now also returns the max value computed within array

--- src/main/java/net/sci/image/process/SobelGradientNorm.java
+++ src/main/java/net/sci/image/process/SobelGradientNorm.java
@@ -9,9 +9,11 @@ import static java.lang.Math.min;
 import net.sci.algo.AlgoStub;
 import net.sci.array.Array;
 import net.sci.array.scalar.Float32Array;
+import net.sci.array.scalar.Scalar;
 import net.sci.array.scalar.ScalarArray;
 import net.sci.array.scalar.ScalarArray2D;
 import net.sci.array.scalar.ScalarArray3D;
+import net.sci.image.Image;
 import net.sci.image.ImageArrayOperator;
 
 /**
@@ -25,31 +27,36 @@ import net.sci.image.ImageArrayOperator;
 public class SobelGradientNorm extends AlgoStub implements ImageArrayOperator
 {
     /**
-     * Creates a new instance of Sobel Gradient operator.
+     * Creates a new instance of Sobel Gradient Norm operator.
      */
     public SobelGradientNorm()
     {
     }
 
-    public void processScalar(ScalarArray<?> input, ScalarArray<?> output)
+    public double processScalar(ScalarArray<?> source, ScalarArray<?> target)
 	{
-		if (input instanceof ScalarArray2D && output instanceof ScalarArray2D)
-		{
-			processScalar2d((ScalarArray2D<?>) input, (ScalarArray2D<?>) output);
-		}
-		else if (input instanceof ScalarArray3D && output instanceof ScalarArray3D)
-		{
-			processScalar3d((ScalarArray3D<?>) input, (ScalarArray3D<?>) output);
-		}
-		else
-		{
-			throw new RuntimeException(
-					"Can not process input of class " + input.getClass()
-							+ " with output of class " + output.getClass());
-		}
+        int nd1 = source.dimensionality();
+        int nd2 = target.dimensionality();
+        if (nd1 != nd2)
+        {
+            throw new IllegalArgumentException("Both arrays must have the same dimensionality");
+        }
+        
+        if (!net.sci.array.Arrays.isSameSize(source, target))
+        {
+            throw new IllegalArgumentException("Both arrays must have the same size");
+        }
+        
+        // Choose the most appropriate implementation, depending on array dimensions
+        return switch (nd1)
+        {
+            case 2 -> processScalar2d(ScalarArray2D.wrap(source), ScalarArray2D.wrap(target));
+            case 3 -> processScalar3d(ScalarArray3D.wrap(source), ScalarArray3D.wrap(target));
+            default -> throw new IllegalArgumentException("Can process Sobel Gradient only on 2D or 3D images");
+        };
 	}
 
-	public void processScalar2d(ScalarArray2D<?> source, ScalarArray2D<?> target)
+	public double processScalar2d(ScalarArray2D<?> source, ScalarArray2D<?> target)
 	{
 		int sizeX = source.size(0);
 		int sizeY = source.size(1);
@@ -57,16 +64,18 @@ public class SobelGradientNorm extends AlgoStub implements ImageArrayOperator
 		float[][] maskGradX = { { -1, 0, 1 }, { -2, 0, 2 }, { -1, 0, 1 } };
 		float[][] maskGradY = { { -1, -2, -1 }, { 0, 0, 0 }, { 1, 2, 1 } };
 
-		// accumulators for gradients in X and Y directions
-		double gx, gy;
-
+		// accumulator for max value of gradient norm
+		double hMax = 0.0; 
+		
+		// iterate over pixels
 		for (int y = 0; y < sizeY; y++)
 		{
 			// iterate over pixels of the row
 			for (int x = 0; x < sizeX; x++)
 			{
-				gx = 0;
-				gy = 0;
+                // accumulators for gradients in X and Y directions
+				double gx = 0;
+				double gy = 0;
 
 				// uses replication of border pixel
 				for (int ky = 0; ky < 3; ky++)
@@ -83,16 +92,19 @@ public class SobelGradientNorm extends AlgoStub implements ImageArrayOperator
 					}
 				}
 
-				gx = gx / 4;
-				gy = gy / 4;
-
 				// set up value of gradient norm
-				target.setValue(x, y, Math.hypot(gx, gy));
+				double h = Math.hypot(gx / 4, gy / 4); 
+				target.setValue(x, y, h);
+				
+				// update max over result array
+				if (h > hMax) hMax = h;
 			}
 		}
+		
+		return hMax;
 	}
 
-	public void processScalar3d(ScalarArray3D<?> source, ScalarArray3D<?> target)
+	public double processScalar3d(ScalarArray3D<?> source, ScalarArray3D<?> target)
 	{
 		// get array size
 		int sizeX = source.size(0);
@@ -113,9 +125,10 @@ public class SobelGradientNorm extends AlgoStub implements ImageArrayOperator
 				{ { 0, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 } }, 			// z =  0
 				{ { 1, 2, 1 }, { 2, 4, 2 }, { 1, 2, 1 } } }; 		// z = +1
 
-		// accumulators for gradients in X, Y and Z directions
-		double gx, gy, gz;
-
+        // accumulator for max value of gradient norm
+        double hMax = 0.0; 
+        
+        // iterate over pixels
 		for (int z = 0; z < sizeZ; z++)
 		{
 			for (int y = 0; y < sizeY; y++)
@@ -123,9 +136,10 @@ public class SobelGradientNorm extends AlgoStub implements ImageArrayOperator
 				// iterate over pixels of the row
 				for (int x = 0; x < sizeX; x++)
 				{
-					gx = 0;
-					gy = 0;
-					gz = 0;
+				    // accumulators for gradients in X, Y and Z directions
+			        double gx = 0;
+					double gy = 0;
+					double gz = 0;
 
 					// uses replication of border pixel
 					for (int kz = 0; kz < 3; kz++)
@@ -153,12 +167,43 @@ public class SobelGradientNorm extends AlgoStub implements ImageArrayOperator
 					gz = gz / 16;
 
 					// set up values of gradient norm
-					target.setValue(x, y, z, Math.sqrt(gx * gx + gy * gy + gz * gz));
+					double h = Math.sqrt(gx * gx + gy * gy + gz * gz);
+					target.setValue(x, y, z, h);
+	                
+	                // update max over result array
+	                if (h > hMax) hMax = h;
 				}
 			}
 		}
+		
+		return hMax;
 	}
 
+    /**
+     * Overrides the default implementation to update display range at the end
+     * of image creation.
+     */
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    @Override
+    public Image process(Image image)
+    {
+        // retrieve input array and check validity
+        Array<?> array = image.getData();
+        if (!(array.sampleElement() instanceof Scalar<?>))
+        {
+            throw new IllegalArgumentException("Requires a scalar array as input");
+        }
+        
+        // apply processing on a newly created result array
+        ScalarArray<?> result = createEmptyOutputArray(array);
+        double maxVal = processScalar(ScalarArray.wrap((Array<Scalar>) array), result);
+        
+        // create result image and update default display
+        Image resImage = new Image(result, image);
+        resImage.getDisplaySettings().setDisplayRange(new double[] {0, maxVal});
+        return resImage;
+    }
+    
     @Override
     public <T> Array<?> process(Array<T> array)
     {
@@ -166,12 +211,12 @@ public class SobelGradientNorm extends AlgoStub implements ImageArrayOperator
         {
             throw new IllegalArgumentException("Requires a scalar array as input");
         }
-        ScalarArray<?> result = createEmptyOutputArray((ScalarArray<?>) array);
+        ScalarArray<?> result = createEmptyOutputArray(array);
         processScalar((ScalarArray<?>) array, result);
         return result;
     }
 
-    public ScalarArray<?> createEmptyOutputArray(ScalarArray<?> array)
+    public ScalarArray<?> createEmptyOutputArray(Array<?> array)
     {
         return Float32Array.create(array.size());
     }
