58e712bf8e6eb99ffbfe51b46ceb49a9383926c0
dlegland <david.legland@inrae.fr>
2024-09-30 10:54:01 +0200

    image/analyze: add tests for IntrinsicVolumesXD

--- src/test/java/net/sci/image/analyze/region2d/IntrinsicVolumes2DTest.java
+++ src/test/java/net/sci/image/analyze/region2d/IntrinsicVolumes2DTest.java
@@ -5,9 +5,13 @@ package net.sci.image.analyze.region2d;
 
 import static org.junit.Assert.*;
 
+import java.util.Map;
+
 import org.junit.Test;
 
 import net.sci.array.binary.BinaryArray2D;
+import net.sci.array.numeric.IntArray2D;
+import net.sci.array.numeric.UInt8Array2D;
 import net.sci.image.Calibration;
 
 /**
@@ -38,5 +42,66 @@ public class IntrinsicVolumes2DTest
         assertEquals(314.15, result.area, 3.0);
         assertEquals(62.82, result.perimeter, 0.6);
         assertEquals(1.0, result.eulerNumber, 0.01);
+    }
+    
+    /**
+     * Test method for {@link net.sci.image.analyze.region2d.RegionAnalyzer2D#analyzeRegions(net.sci.array.numeric.IntArray2D, net.sci.image.Calibration)}.
+     */
+    @Test
+    public final void test_analyzeRegions_allLabels()
+    {
+        IntArray2D<?> array = createFourRegionsLabelMap();
+        
+        IntrinsicVolumes2D algo = new IntrinsicVolumes2D();
+        Map<Integer, IntrinsicVolumes2D.Result> map = algo.analyzeRegions(array, new Calibration(2));
+        
+        assertEquals(map.size(), 4);
+        IntrinsicVolumes2D.Result res3 = map.get(3);
+        assertEquals(res3.area, 1, 0.01);
+        assertEquals(res3.eulerNumber, 1, 0.01);
+        IntrinsicVolumes2D.Result res9 = map.get(9);
+        assertEquals(res9.area, 16, 0.01);
+        assertEquals(res9.eulerNumber, 1, 0.01);
+    }
+    
+    /**
+     * Test method for {@link net.sci.image.analyze.region2d.RegionAnalyzer2D#analyzeRegions(net.sci.array.numeric.IntArray2D, int[], net.sci.image.Calibration)}.
+     */
+    @Test
+    public final void test_analyzeRegions_selectedLabels()
+    {
+        IntArray2D<?> array = createFourRegionsLabelMap();
+        int[] labels = new int[] {3, 5, 9};
+        
+        IntrinsicVolumes2D algo = new IntrinsicVolumes2D();
+        IntrinsicVolumes2D.Result[] results = algo.analyzeRegions(array, labels, new Calibration(2));
+        
+        assertEquals(results.length, 3);
+        IntrinsicVolumes2D.Result res3 = results[0];
+        assertEquals(res3.area, 1, 0.01);
+        assertEquals(res3.eulerNumber, 1, 0.01);
+        IntrinsicVolumes2D.Result res9 = results[2];
+        assertEquals(res9.area, 16, 0.01);
+        assertEquals(res9.eulerNumber, 1, 0.01);
+    }
+    
+    /**
+     * Creates a label map containing four regions.
+     * 
+     * @return a label map containing four regions.
+     */
+    private static final UInt8Array2D createFourRegionsLabelMap()
+    {
+        UInt8Array2D array = UInt8Array2D.fromIntArray(new int[][] {
+            {0, 0, 0, 0, 0, 0, 0, 0}, 
+            {0, 3, 0, 5, 5, 5, 5, 0}, 
+            {0, 0, 0, 0, 0, 0, 0, 0}, 
+            {0, 8, 0, 9, 9, 9, 9, 0}, 
+            {0, 8, 0, 9, 9, 9, 9, 0}, 
+            {0, 8, 0, 9, 9, 9, 9, 0}, 
+            {0, 8, 0, 9, 9, 9, 9, 0}, 
+            {0, 0, 0, 0, 0, 0, 0, 0}, 
+        });
+        return array;
     }
 }
--- src/test/java/net/sci/image/analyze/region3d/IntrinsicVolumes3DTest.java
+++ src/test/java/net/sci/image/analyze/region3d/IntrinsicVolumes3DTest.java
@@ -5,9 +5,13 @@ package net.sci.image.analyze.region3d;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Map;
+
 import org.junit.Test;
 
 import net.sci.array.binary.BinaryArray3D;
+import net.sci.array.numeric.IntArray3D;
+import net.sci.array.numeric.UInt8Array3D;
 import net.sci.image.Calibration;
 
 /**
@@ -40,5 +44,98 @@ public class IntrinsicVolumes3DTest
         assertEquals(1256.6, result.surfaceArea, 60.0);
         assertEquals(20.0, result.meanBreadth, 1.0);
         assertEquals(1.0, result.eulerNumber, 0.01);
+    }
+    
+    /**
+     * Test method for {@link net.sci.image.analyze.region3d.RegionAnalyzer3D#analyzeRegions(net.sci.array.numeric.IntArray3D, net.sci.image.Calibration)}.
+     */
+    @Test
+    public final void test_analyzeRegions_allLabels()
+    {
+        IntArray3D<?> array = createEightRegionsLabelMap();
+        
+        IntrinsicVolumes3D algo = new IntrinsicVolumes3D();
+        Map<Integer, IntrinsicVolumes3D.Result> map = algo.analyzeRegions(array, new Calibration(3));
+        
+        assertEquals(map.size(), 8);
+        IntrinsicVolumes3D.Result res3 = map.get(3);
+        assertEquals(res3.volume, 1, 0.01);
+        assertEquals(res3.eulerNumber, 1, 0.01);
+        IntrinsicVolumes3D.Result res9 = map.get(17);
+        assertEquals(res9.volume, 64, 0.01);
+        assertEquals(res9.eulerNumber, 1, 0.01);
+    }
+    
+    /**
+     * Test method for {@link net.sci.image.analyze.region3d.RegionAnalyzer3D#analyzeRegions(net.sci.array.numeric.IntArray3D, int[], net.sci.image.Calibration)}.
+     */
+    @Test
+    public final void test_analyzeRegions_selectedLabels()
+    {
+        IntArray3D<?> array = createEightRegionsLabelMap();
+        int[] labels = new int[] {3, 5, 9, 13, 17};
+        
+        IntrinsicVolumes3D algo = new IntrinsicVolumes3D();
+        IntrinsicVolumes3D.Result[] results = algo.analyzeRegions(array, labels, new Calibration(3));
+        
+        assertEquals(results.length, 5);
+        IntrinsicVolumes3D.Result res3 = results[0];
+        assertEquals(res3.volume, 1, 0.01);
+        assertEquals(res3.eulerNumber, 1, 0.01);
+        IntrinsicVolumes3D.Result res5 = results[1];
+        assertEquals(res5.volume, 4, 0.01);
+        assertEquals(res5.eulerNumber, 1, 0.01);
+        IntrinsicVolumes3D.Result res9 = results[2];
+        assertEquals(res9.volume, 16, 0.01);
+        assertEquals(res9.eulerNumber, 1, 0.01);
+        IntrinsicVolumes3D.Result res17 = results[4];
+        assertEquals(res17.volume, 64, 0.01);
+        assertEquals(res17.eulerNumber, 1, 0.01);
+    }
+    
+    
+    /**
+     * Creates a label map containing eight regions, with labels
+     * 3, 5, 7, 9, 11, 13, 15, 17.
+     * 
+     * @return a label map containing eight regions.
+     */
+    private static final UInt8Array3D createEightRegionsLabelMap()
+    {
+        UInt8Array3D array = UInt8Array3D.create(8, 8, 8);
+        
+        // single voxel region
+        array.setInt(1, 1, 1, 3);
+        
+        // lines of four voxels
+        for (int i = 3; i < 7; i++)
+        {
+            array.setInt(i, 1, 1,  5);
+            array.setInt(1, i, 1,  7);
+            array.setInt(1, 1, i, 11);
+        }
+        
+        // 4x4 voxels planes
+        for (int i = 3; i < 7; i++)
+        {
+            for (int j = 3; j < 7; j++)
+            {
+                array.setInt(i, j, 1,  9);
+                array.setInt(i, 1, j, 13);
+                array.setInt(1, i, j, 15);
+            }
+        }        
+        // 4x4x4 cubic region
+        for (int i = 3; i < 7; i++)
+        {
+            for (int j = 3; j < 7; j++)
+            {
+                for (int k = 3; k < 7; k++)
+                {
+                    array.setInt(i, j, k, 17);
+                }
+            }
+        }        
+        return array;
     }
 }
