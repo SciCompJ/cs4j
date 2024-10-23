53b4b195841b8d15f253ef1e6a3d24fe2a85d95d
dlegland <david.legland@inrae.fr>
2024-10-23 23:49:15 +0200

    add Aggregate for Table

---
+++ src/main/java/net/sci/table/process/Aggregate.java
@@ -0,0 +1,78 @@
+/**
+ * 
+ */
+package net.sci.table.process;
+
+import net.sci.table.CategoricalColumn;
+import net.sci.table.Column;
+import net.sci.table.NumericColumn;
+import net.sci.table.Table;
+
+/**
+ * Aggregates values of a numeric table based on the levels of a column (with
+ * same number of rows).
+ */
+public class Aggregate
+{
+    public static final Table aggregate(Table table, CategoricalColumn groups)
+    {
+        Column[] columns = new Column[table.columnCount()];
+        for (int c = 0; c < table.columnCount(); c++)
+        {
+            Column column = table.column(c);
+            if (!(column instanceof NumericColumn))
+            {
+                throw new RuntimeException("Column #" + c + " (" + column.getName() + ") is not a numeric column");
+            }
+            columns[c] = aggregate((NumericColumn) column, groups);
+        }
+        
+        Table res = Table.create(columns);
+        String[] levels = groups.levelNames();
+        String[] rowNames = new String[levels.length];
+        for (int i = 0; i < levels.length; i++)
+        {
+            rowNames[i] = groups.getName() + "=" + levels[i];
+        }
+        res.setRowNames(rowNames);
+        res.setName(table.getName()+"-mean");
+        
+        return res;
+    }
+
+    public static final NumericColumn aggregate(NumericColumn column, CategoricalColumn groups)
+    {
+        if (column.length() != groups.length())
+        {
+            throw new RuntimeException("Requires the two columns to have the same length");
+        }
+        String[] levels = groups.levelNames();
+        int nGroups = levels.length;
+        
+        double[] means = new double[nGroups];
+        int[] counts = new int[nGroups];
+        
+        for (int r = 0; r < column.length(); r++)
+        {
+            String level = groups.getName(r);
+            for (int j = 0; j < nGroups; j++)
+            {
+                if (levels[j].equals(level))
+                {
+                    means[j] += column.getValue(r);
+                    counts[j]++;
+                    break;
+                }
+            }
+        }
+        
+        // normalize sums by counts to get the mean
+        for (int j = 0; j < nGroups; j++)
+        {
+            means[j] /= counts[j];
+        }
+        
+        // create result column
+        return NumericColumn.create(column.getName(), means);
+    }
+}
---
+++ src/test/java/net/sci/table/process/AggregateTest.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package net.sci.table.process;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.Test;
+
+import net.sci.table.CategoricalColumn;
+import net.sci.table.NumericTable;
+import net.sci.table.Table;
+import net.sci.table.io.DelimitedTableReader;
+import net.sci.table.io.TableReader;
+
+/**
+ * 
+ */
+public class AggregateTest
+{
+
+    /**
+     * Test method for {@link net.sci.table.process.Aggregate#aggregate(net.sci.table.Table, net.sci.table.CategoricalColumn)}.
+     * @throws IOException 
+     */
+    @Test
+    public final void testAggregateTableCategoricalColumn() throws IOException
+    {
+        String fileName = getClass().getResource("/tables/iris/fisherIris.txt").getFile();
+        File file = new File(fileName);
+        TableReader reader = new DelimitedTableReader();
+        
+        Table table = reader.readTable(file);
+        
+        NumericTable data = NumericTable.keepNumericColumns(table);
+        
+        CategoricalColumn groups = (CategoricalColumn) table.column(4);
+        
+        Table res = Aggregate.aggregate(data, groups);
+        
+        assertEquals(res.rowCount(), 3);
+        assertEquals(res.columnCount(), data.columnCount());
+        
+        res.print();
+    }
+
+}
