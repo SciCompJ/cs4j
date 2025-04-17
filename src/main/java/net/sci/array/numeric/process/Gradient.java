d1fd579f76ef6e0d82649abb9e92db4f131a84fc
dlegland <david.legland@inrae.fr>
2025-04-17 11:45:39 +0200

    Gradient operator can now specify factory for result array

--- src/main/java/net/sci/array/numeric/process/Gradient.java
+++ src/main/java/net/sci/array/numeric/process/Gradient.java
@@ -12,12 +12,101 @@ import net.sci.array.numeric.ScalarArray;
 import net.sci.array.numeric.VectorArray;
 
 /**
+ * Computes the gradient vector for each element of a scalar array, by computing
+ * finite differences along each dimension.
+ * 
+ * {@snippet lang="java":
+ * UInt8Array2D array = UInt8Array2D.create(20, 20);
+ * array.fillInts((x,y) -> Math.hypot(x-10, y-10) < 6 ? 100 : 0);
+ * Gradient op = new Gradient();
+ * // optional: setup factory
+ * op.setFactory(Float32VectorArray.defaultFactory);
+ * VectorArray<?,?> grad = op.process(array);
+ * // compute norm of gradient
+ * ScalarArray<?> norm = new VectorArrayL2Norm().process(grad);
+ * new Image(norm).show(); 
+ * }
+ * 
+ * @see FiniteDifferences
+ * 
  * @author dlegland
- *
  */
 public class Gradient extends AlgoStub implements ArrayOperator
 {
+    /**
+     * Default empty constructor.
+     */
+    public Gradient()
+    {
+    }
+    
+    /**
+     * The factory used to create output array.
+     */
+    protected VectorArray.Factory<?,?> factory = Float32VectorArray.defaultFactory;
 
+    /**
+     * Computes gradient of the elements in the source array, given as a
+     * ScalarArray, and stores the result in the second input argument, given as
+     * a VectorArray. Returns the reference to the target array.
+     * 
+     * @param source
+     *            the input scalar array
+     * @param target
+     *            the output vector array
+     * @return the reference to the target array
+     */
+    public VectorArray<?,?> processScalar(ScalarArray<?> source, VectorArray<?,?> target)
+    {
+        // check dimensionality of inputs
+        checkArrayDimensions(source, target);
+
+        int nd = source.dimensionality();
+        for (int d = 0; d < nd; d++)
+        {
+            FiniteDifferences diffOp = new FiniteDifferences(d);
+            diffOp.processScalar(source, target.channel(d));
+        }
+        
+        return target;
+    }
+    
+    /**
+     * Changes the factory used to create output arrays. Default is
+     * {@code Float32VectorArray.defaultFactory}.
+     * 
+     * @param factory
+     *            the factory to set
+     */
+    public void setFactory(VectorArray.Factory<?,?> factory)
+    {
+        this.factory = factory;
+    }
+
+    private void checkArrayDimensions(ScalarArray<?> source, VectorArray<?,?> target)
+    {
+        if (target.channelCount() != source.dimensionality())
+        {
+            throw new IllegalArgumentException("Target array must have at least " + source.dimensionality() + " channels");
+        }
+        if (!Arrays.isSameSize(source, target))
+        {
+            throw new IllegalArgumentException("Source and Target arrays must have same dimensions");
+        }
+    }
+
+    /**
+     * Computes gradient of the specified input scalar array.
+     * 
+     * @param array
+     *            the input scalar array
+     * @return the gradient of the array, as an instance of {@code VectorArray}
+     */
+    public VectorArray<?,?> processScalar(ScalarArray<?> array)
+    {
+        return processScalar(array, factory.create(array.size(), array.dimensionality()));
+    }
+    
     /* (non-Javadoc)
      * @see net.sci.array.ArrayOperator#process(net.sci.array.Array)
      */
@@ -32,36 +121,10 @@ public class Gradient extends AlgoStub implements ArrayOperator
         
         // allocate memory for result
         ScalarArray<?> source = (ScalarArray<?>) array;
-        VectorArray<?,?> target = Float32VectorArray.create(source.size(), source.dimensionality());
+        VectorArray<?,?> target = factory.create(source.size(), source.dimensionality());
         
         processScalar(source, target);
         
         return target;
     }
-
-    public void processScalar(ScalarArray<?> source, VectorArray<?,?> target)
-    {
-        // check dimensionality of inputs
-        checkArrayDimensions(source, target);
-
-        int nd = source.dimensionality();
-        for (int d = 0; d < nd; d++)
-        {
-            FiniteDifferences diffOp = new FiniteDifferences(d);
-            diffOp.processScalar(source, target.channel(d));
-        }
-    }
-    
-    private void checkArrayDimensions(ScalarArray<?> source, VectorArray<?,?> target)
-    {
-        if (target.channelCount() != source.dimensionality())
-        {
-            throw new IllegalArgumentException("Target array must have at least " + source.dimensionality() + " channels");
-        }
-        if (!Arrays.isSameSize(source, target))
-        {
-            throw new IllegalArgumentException("Source and Target arrays must have same dimensions");
-        }
-    }
-
 }
--- src/test/java/net/sci/array/numeric/process/GradientTest.java
+++ src/test/java/net/sci/array/numeric/process/GradientTest.java
@@ -4,10 +4,13 @@
 package net.sci.array.numeric.process;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 import net.sci.array.numeric.Float32Array2D;
+import net.sci.array.numeric.Float64Vector;
+import net.sci.array.numeric.Float64VectorArray;
 import net.sci.array.numeric.ScalarArray;
 import net.sci.array.numeric.VectorArray;
 
@@ -23,17 +26,10 @@ public class GradientTest
     @Test
     public final void testProcess()
     {
-        Float32Array2D array = Float32Array2D.create(10,  10);
-        for (int y = 2; y < 8; y++)
-        {
-            for (int x = 2; x < 8; x++)
-            {
-                array.setValue(x, y, 1.0);
-            }
-        }
+        Float32Array2D array = createInputArray();
 
-        Gradient op0 = new Gradient();
-        VectorArray<?,?> grad = (VectorArray<?,?>) op0.process(array);
+        Gradient op = new Gradient();
+        VectorArray<?,?> grad = (VectorArray<?,?>) op.process(array);
 
         ScalarArray<?> diff0 = grad.channel(0);
         assertEquals(diff0.getValue(new int[] {0, 5}),  0.0, .001);
@@ -48,5 +44,48 @@ public class GradientTest
         assertEquals(diff1.getValue(new int[] {5, 5}),  0.0, .001);
         assertEquals(diff1.getValue(new int[] {5, 7}), -0.5, .001);
         assertEquals(diff1.getValue(new int[] {5, 9}),  0.0, .001);
+    }
+    
+    /**
+     * Test method for {@link net.sci.array.numeric.process.Gradient#process(net.sci.array.Array)}.
+     */
+    @Test
+    public final void testProcess_Float64VectorOutput()
+    {
+        Float32Array2D array = createInputArray();
+
+        Gradient op = new Gradient();
+        op.setFactory(Float64VectorArray.defaultFactory);
+        VectorArray<?,?> grad = (VectorArray<?,?>) op.process(array);
+        
+        assertTrue(grad.sampleElement() instanceof Float64Vector);
+        assertTrue(grad instanceof Float64VectorArray);
+
+        ScalarArray<?> diff0 = grad.channel(0);
+        assertEquals(diff0.getValue(new int[] {0, 5}),  0.0, .001);
+        assertEquals(diff0.getValue(new int[] {2, 5}),  0.5, .001);
+        assertEquals(diff0.getValue(new int[] {5, 5}),  0.0, .001);
+        assertEquals(diff0.getValue(new int[] {7, 5}), -0.5, .001);
+        assertEquals(diff0.getValue(new int[] {9, 5}),  0.0, .001);
+
+        ScalarArray<?> diff1 = grad.channel(1);
+        assertEquals(diff1.getValue(new int[] {5, 0}),  0.0, .001);
+        assertEquals(diff1.getValue(new int[] {5, 2}),  0.5, .001);
+        assertEquals(diff1.getValue(new int[] {5, 5}),  0.0, .001);
+        assertEquals(diff1.getValue(new int[] {5, 7}), -0.5, .001);
+        assertEquals(diff1.getValue(new int[] {5, 9}),  0.0, .001);
+    }
+    
+    private static final Float32Array2D createInputArray()
+    {
+        Float32Array2D array = Float32Array2D.create(10,  10);
+        for (int y = 2; y < 8; y++)
+        {
+            for (int x = 2; x < 8; x++)
+            {
+                array.setValue(x, y, 1.0);
+            }
+        }
+        return array;
     }
 }
