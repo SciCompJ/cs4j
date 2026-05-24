c359f2d947479d8f9765b72cbf9ab6a7cfcbd732
dlegland <david.legland@inrae.fr>
2026-05-24 22:51:12 +0200

    refactor computation of boundary points from 2D/3D binary image

--- src/main/java/net/sci/image/vectorize/BinaryImageBoundaryFacetMidPoints.java
+++ src/main/java/net/sci/image/vectorize/BinaryImageBoundaryFacetMidPoints.java
@@ -10,8 +10,12 @@ import java.util.TreeMap;
 import java.util.TreeSet;
 
 import net.sci.algo.AlgoStub;
+import net.sci.array.Array;
+import net.sci.array.binary.Binary;
+import net.sci.array.binary.BinaryArray;
 import net.sci.array.binary.BinaryArray2D;
 import net.sci.array.binary.BinaryArray3D;
+import net.sci.geom.Point;
 import net.sci.geom.geom2d.Point2D;
 import net.sci.geom.geom3d.Point3D;
 
@@ -19,55 +23,11 @@ import net.sci.geom.geom3d.Point3D;
  * Computes the collection of boundary points from a binary image.
  * 
  * For 2D binary images, returns the coordinates of the points located at the
- * middle of boundary edges. Points are stored in a map of x-coordinates indexed
- * by the y-coordinate. This structure allows to efficiently compute related
- * structures such as convex hulls or bounding boxes.
- * 
- * For 3D binary images, returns the coordinates of the points located at the
- * middle of boundary faces. Points are stored in a doubly-nested Map data
- * structure, in voxel coordinates.
+ * middle of boundary edges. For 3D binary images, returns the coordinates of
+ * the points located at the middle of boundary faces.
  */
 public class BinaryImageBoundaryFacetMidPoints extends AlgoStub
 {
-    /**
-     * Converts the data structure returned by the {@code processBinary2d}
-     * method into a list of points corresponding to the middle points of the
-     * boundary edges of pixels within a binary image.
-     * 
-     * @param map
-     *            a map of x-coordinates indexed by y-coordinates
-     * @return a list of instances of Point2D
-     */
-    public static final List<Point2D> reduceMap2d(Map<Double, TreeSet<Double>> map)
-    {
-        int n = map.values().stream().mapToInt(set -> set.size()).sum();
-        ArrayList<Point2D> list = new ArrayList<Point2D>(n);
-        map.keySet().forEach(y -> map.get(y).forEach(x -> list.add(Point2D.of(x, y))));
-        return list;
-    }
-    
-    /**
-     * Converts the data structure returned by the {@code processBinary3d}
-     * method into a list of 3D points corresponding to the middle points of the
-     * boundary faces of voxels within a binary image.
-     * 
-     * @param map
-     *            a map of map of x-coordinates indexed by y-coordinates, indexed by z-coordinates
-     * @return a list of instances of Point2D
-     */
-    public static final List<Point3D> reduceMap3d(TreeMap<Double, TreeMap<Double, TreeSet<Double>>> map)
-    {
-        int n = map.values().stream().mapToInt(set -> set.size()).sum();
-        ArrayList<Point3D> list = new ArrayList<Point3D>(n);
-        map.keySet().forEach(z -> 
-        {
-            TreeMap<Double, TreeSet<Double>> map2 = map.get(z);
-            map2.keySet().forEach(y -> 
-                map2.get(y).forEach(x -> list.add(Point3D.of(x, y, z))));
-        });
-        return list;
-    }
-    
     /**
      * Default empty constructor.
      */
@@ -75,7 +35,57 @@ public class BinaryImageBoundaryFacetMidPoints extends AlgoStub
     {
     }
     
-    public TreeMap<Double, TreeSet<Double>> processBinary2d(BinaryArray2D array)
+    /**
+     * Computes the collection of boundary points from a binary array. The input
+     * array is expected to contain binary elements. Boundary points are located
+     * at the middle of the boundary facets.
+     *
+     * @param array
+     *            the input array (2D or 3D array of binary elements expected)
+     * @return the collection of boundary points, as a list of points.
+     * @throws RuntimeException if the array does not contain elements, or if the dimensionality is not managed.
+     */
+    public List<? extends Point> process(Array<?> array)
+    {
+        if (array.elementClass() != Binary.class)
+        {
+            throw new RuntimeException("Requires an array containing binary elements, not " + array.elementClass().getName());
+        }
+        
+        BinaryArray binaryArray = BinaryArray.wrap(array);
+        return switch(binaryArray.dimensionality())
+        {
+            case 2 -> processBinary2d(BinaryArray2D.wrap(binaryArray));
+            case 3 -> processBinary3d(BinaryArray3D.wrap(binaryArray));
+            default -> throw new RuntimeException(
+                    "Can not process array with dimensionality " + binaryArray.dimensionality());
+        };
+    }
+    
+    /**
+     * Computes the collection of boundary points from a 2D binary image.
+     * Boundary points are located at the middle of the boundary edges, making
+     * each pixel having a "diamond" shape.
+     *
+     * @param array
+     *            the 2D binary array
+     * @return the collection of boundary points, as a list of Point2D.
+     */
+    public List<Point2D> processBinary2d(BinaryArray2D array)
+    {
+        return reduceMap2d(computeMap2d(array));
+    }
+    
+    /**
+     * Computes the collection of boundary points from a 2D binary array, by
+     * returning the result as a map of x-coordinates indexed by the
+     * y-coordinate. This structure allows to efficiently compute related
+     * structures such as convex hulls or bounding boxes.
+     * 
+     * @param array
+     * @return
+     */
+    public TreeMap<Double, TreeSet<Double>> computeMap2d(BinaryArray2D array)
     {
         // retrieve label map data
         int sizeX = array.size(0);
@@ -102,16 +112,10 @@ public class BinaryImageBoundaryFacetMidPoints extends AlgoStub
                 upPixel = x < sizeX & y > 0 ? array.getBoolean(x, y - 1) : false;
 
                 // check boundary with upper pixel
-                if (upPixel != currentPixel)
-                {
-                    addPoint(pointMaps, x, y - 0.5);
-                }
+                if (upPixel != currentPixel) addPoint(pointMaps, x, y - 0.5);
                 
                 // check boundary with left pixel
-                if (leftPixel != currentPixel)
-                {
-                    addPoint(pointMaps, x - 0.5, y);
-                }
+                if (leftPixel != currentPixel) addPoint(pointMaps, x - 0.5, y);
 
                 // update values of left label for next iteration
                 leftPixel = currentPixel;
@@ -132,7 +136,57 @@ public class BinaryImageBoundaryFacetMidPoints extends AlgoStub
         map.put(y, set);
     }
     
-    public TreeMap<Double, TreeMap<Double, TreeSet<Double>>> processBinary3d(BinaryArray3D array)
+    /**
+     * Converts the data structure returned by the {@code processBinary2d}
+     * method into a list of points corresponding to the middle points of the
+     * boundary edges of pixels within a binary image.
+     * 
+     * @param map
+     *            a map of x-coordinates indexed by y-coordinates
+     * @return a list of instances of Point2D
+     */
+    public static final List<Point2D> reduceMap2d(Map<Double, TreeSet<Double>> map)
+    {
+        int n = map.values().stream().mapToInt(set -> set.size()).sum();
+        ArrayList<Point2D> list = new ArrayList<Point2D>(n);
+        map.keySet().forEach(y -> map.get(y).forEach(x -> list.add(Point2D.of(x, y))));
+        return list;
+    }
+
+    /**
+     * Computes the collection of boundary points from a 3D binary image.
+     * Boundary points are located at the middle of the boundary edges, making
+     * each voxel having an octahedron shape.
+     *
+     * @param array
+     *            the 3D binary array
+     * @return the collection of boundary points, as a list of Point3D.
+     */
+    public List<Point3D> processBinary3d(BinaryArray3D array)
+    {
+        return reduceMap3d(computeMap3d(array));
+    }
+    
+    /**
+     * Computes the collection of boundary points from a 3D binary array, by
+     * returning the result in a nested Map data structure. This structure can
+     * be used to efficiently compute related structures such as convex hulls or
+     * bounding boxes.
+     * 
+     * Results are organized as follow:
+     * <ul>
+     * <li>The keys of the first map correspond to the z-coordinates of the
+     * boundary points.</li>
+     * <li>The associated value is also a map, using the y-coordinates of the
+     * boundary points as keys</li>
+     * <li>The associated value of the second map is a set of
+     * x-coordinates.</li>
+     * </ul>
+     * 
+     * @param array the binary array to process
+     * @return the coordinates of the boundary points in a nested map structure. 
+     */
+    public TreeMap<Double, TreeMap<Double, TreeSet<Double>>> computeMap3d(BinaryArray3D array)
     {
         // retrieve label map data
         int sizeX = array.size(0);
@@ -159,25 +213,13 @@ public class BinaryImageBoundaryFacetMidPoints extends AlgoStub
                 {
                     // update pixel values of configuration
                     currentVoxel = x < sizeX && y < sizeY && z < sizeZ ? array.getBoolean(x, y, z) : false;
-                    upVoxel = x < sizeX && y > 0  && z < sizeZ ? array.getBoolean(x, y - 1, z) : false;
                     topVoxel = x < sizeX && y < sizeY  && z > 0 ? array.getBoolean(x, y, z - 1) : false;
+                    upVoxel = x < sizeX && y > 0  && z < sizeZ ? array.getBoolean(x, y - 1, z) : false;
                     
-                    if (topVoxel != currentVoxel)
-                    {
-                        addPoint(pointMaps, x, y, z - 0.5);
-                    }
-                    
-                    // check boundary with upper pixel
-                    if (upVoxel != currentVoxel)
-                    {
-                        addPoint(pointMaps, x, y - 0.5, z);
-                    }
-                    
-                    // check boundary with left pixel
-                    if (leftVoxel != currentVoxel)
-                    {
-                        addPoint(pointMaps, x - 0.5, y, z);
-                    }
+                    // check boundary by comparing current voxel with neighbors
+                    if (topVoxel != currentVoxel) addPoint(pointMaps, x, y, z - 0.5);
+                    if (upVoxel != currentVoxel) addPoint(pointMaps, x, y - 0.5, z);
+                    if (leftVoxel != currentVoxel) addPoint(pointMaps, x - 0.5, y, z);
                     
                     // update values of left label for next iteration
                     leftVoxel = currentVoxel;
@@ -208,6 +250,28 @@ public class BinaryImageBoundaryFacetMidPoints extends AlgoStub
         set.add(x);
         mapZ.put(y, set);
         map.put(z, mapZ);
+    }
+
+    /**
+     * Converts the data structure returned by the {@code processBinary3d}
+     * method into a list of 3D points corresponding to the middle points of the
+     * boundary faces of voxels within a binary image.
+     * 
+     * @param map
+     *            a map of map of x-coordinates indexed by y-coordinates, indexed by z-coordinates
+     * @return a list of instances of Point2D
+     */
+    public static final List<Point3D> reduceMap3d(Map<Double, TreeMap<Double, TreeSet<Double>>> map)
+    {
+        int n = map.values().stream().mapToInt(set -> set.size()).sum();
+        ArrayList<Point3D> list = new ArrayList<Point3D>(n);
+        map.keySet().forEach(z -> 
+        {
+            TreeMap<Double, TreeSet<Double>> map2 = map.get(z);
+            map2.keySet().forEach(y -> 
+                map2.get(y).forEach(x -> list.add(Point3D.of(x, y, z))));
+        });
+        return list;
     }
 
 }
--- src/test/java/net/sci/image/vectorize/BinaryImageBoundaryFacetMidPointsTest.java
+++ src/test/java/net/sci/image/vectorize/BinaryImageBoundaryFacetMidPointsTest.java
@@ -10,7 +10,9 @@ import java.util.List;
 import org.junit.Test;
 
 import net.sci.array.binary.BinaryArray2D;
+import net.sci.array.binary.BinaryArray3D;
 import net.sci.geom.geom2d.Point2D;
+import net.sci.geom.geom3d.Point3D;
 
 /**
  * 
@@ -32,7 +34,7 @@ public class BinaryImageBoundaryFacetMidPointsTest
         });
         
         BinaryImageBoundaryFacetMidPoints algo = new BinaryImageBoundaryFacetMidPoints();
-        List<Point2D> pts = BinaryImageBoundaryFacetMidPoints.reduceMap2d(algo.processBinary2d(array));
+        List<Point2D> pts = algo.processBinary2d(array);
         
         assertEquals(10, pts.size());
     }
@@ -50,9 +52,54 @@ public class BinaryImageBoundaryFacetMidPointsTest
         });
         
         BinaryImageBoundaryFacetMidPoints algo = new BinaryImageBoundaryFacetMidPoints();
-        List<Point2D> pts = BinaryImageBoundaryFacetMidPoints.reduceMap2d(algo.processBinary2d(array));
+        List<Point2D> pts = algo.processBinary2d(array);
         
         assertEquals(12, pts.size());
+    }
+    
+    /**
+     * Test method for {@link net.sci.image.vectorize.BinaryImageBoundaryFacetMidPoints#processBinary3d(net.sci.array.binary.BinaryArray3D)}.
+     */
+    @Test
+    public final void test_processBinary3d_cube()
+    {
+        BinaryArray3D array = BinaryArray3D.create(5, 4, 3);
+        array.setBoolean(1, 1, 1, true);
+        array.setBoolean(2, 1, 1, true);
+        array.setBoolean(3, 1, 1, true);
+        array.setBoolean(1, 2, 1, true);
+        array.setBoolean(2, 2, 1, true);
+        array.setBoolean(3, 2, 1, true);
+        
+        BinaryImageBoundaryFacetMidPoints algo = new BinaryImageBoundaryFacetMidPoints();
+        List<Point3D> pts = algo.processBinary3d(array);
+        
+        // cuboid 3x2x1
+        // 2x6 = 12 boundary points in z-direction
+        // 2x3 =  6 boundary points in y-direction
+        // 2x2 =  4 boundary points in x-direction
+        // total: 22
+        assertEquals(22, pts.size());
+    }
+    
+    /**
+     * Test method for {@link net.sci.image.vectorize.BinaryImageBoundaryFacetMidPoints#processBinary2d(net.sci.array.binary.BinaryArray2D)}.
+     */
+    @Test
+    public final void test_processBinary3d_touchBorder()
+    {
+        BinaryArray3D array = BinaryArray3D.create(3, 2, 1);
+        array.setBoolean(0, 0, 0, true);
+        array.setBoolean(1, 0, 0, true);
+        array.setBoolean(2, 0, 0, true);
+        array.setBoolean(0, 1, 0, true);
+        array.setBoolean(1, 1, 0, true);
+        array.setBoolean(2, 1, 0, true);
+        
+        BinaryImageBoundaryFacetMidPoints algo = new BinaryImageBoundaryFacetMidPoints();
+        List<Point3D> pts = algo.processBinary3d(array);
+        
+        assertEquals(22, pts.size());
     }
     
 }
