5db89ca704dabc9b8dfd96d9db292840836f1f9f
dlegland <david.legland@inrae.fr>
2026-08-21 17:13:37 +0200

    update ImageSlicer

--- src/main/java/net/sci/image/shape/ImageSlicer.java
+++ src/main/java/net/sci/image/shape/ImageSlicer.java
@@ -6,22 +6,22 @@ package net.sci.image.shape;
 import net.sci.array.Array;
 import net.sci.array.Array2D;
 import net.sci.array.Array3D;
+import net.sci.array.shape.Slice;
 import net.sci.image.Calibration;
 import net.sci.image.Image;
 
 /**
- * Encapsulates Array Slicer to also manage image meta data.
+ * Encapsulates Array Slice operator to also manage image meta data.
  * 
+ * @see net.sci.array.shape.Slice
  * @author dlegland
- *
  */
 public class ImageSlicer
 {
     /**
-     * Computes the planar z-slice from the specified 3D image and the slice
-     * index (in z-direction).
+     * Computes a view on the planar z-slice from the specified 3D image and the
+     * slice index (in z-direction). The input image must contain a 3D array.
      * 
-     *  
      * @param image
      *            the input 3D image
      * @param sliceIndex
@@ -35,8 +35,7 @@ public class ImageSlicer
         Array3D<T> array = Array3D.wrap((Array<T>) image.getData());
         
         // extract slice (duplicate it to enforce an array with correct type)
-        Array2D<T> slice = array.slice(sliceIndex).duplicate();
-//        Array2D<T> slice = array.slice(sliceIndex); // TODO: should be able to avoid duplication
+        Array2D<T> slice = array.slice(sliceIndex);
         
         // convert to an image with same type
         Image resultImage = new Image(slice, image.getType(), image);
@@ -50,21 +49,22 @@ public class ImageSlicer
     }
 
     /**
-     * Create a 2D slice from a 3D image, specifying the axes of the slice.
+     * Creates a 2D slice from an image, specifying the two axes of the slice.
      * 
      * @param <T>
      *            the type of the array contained within this image
      * @param image
-     *            the input 3D image
+     *            the input image
      * @param dim1
-     *            the direction of the 3D image corresponding to first slice
+     *            the direction of the input image corresponding to first slice
      *            axis
      * @param dim2
-     *            the direction of the 3D image corresponding to second slice
+     *            the direction of the input image corresponding to second slice
      *            axis
      * @param refPos
-     *            the position of a 3D point belonging to the slice
-     * @return the resulting slice
+     *            the n-dimensional position in the original image of a point
+     *            belonging to the slice
+     * @return the resulting 2D slice image
      */
     public static final <T> Image slice2d(Image image, int dim1, int dim2, int[] refPos)
     {
@@ -84,40 +84,7 @@ public class ImageSlicer
             throw new IllegalArgumentException("Reference position must have as many dimension as input image");
         }
         
-        Array2D<T> resArray;
-        if (array instanceof Array3D && dim1 == 0 && dim2 == 1)
-        {
-            // special case of a 3D array sliced with dimensions X and Y
-            resArray = ((Array3D<T>) array).slice(refPos[2]);
-        }
-        else
-        {
-            // create position pointer for source image
-            int[] srcPos = new int[nd];
-            System.arraycopy(refPos, 0, srcPos, 0, nd);
-
-            // create output
-            int sizeX = image.getSize(dim1);
-            int sizeY = image.getSize(dim2);
-
-            // allocate memory for result
-            resArray = Array2D.wrap(array.newInstance(new int[]{sizeX, sizeY}));
-
-            // iterate over position in target image
-            for (int y = 0; y < sizeY; y++)
-            {
-                srcPos[dim2] = y;
-
-                for (int x = 0; x < sizeX; x++)
-                {
-                    srcPos[dim1] = x;
-
-                    // copy value of selected position
-                    resArray.set(x, y, array.get(srcPos));
-                }
-            }
-        }
-     
+        Array<T> resArray = new Slice(new int[] {dim1, dim2}, refPos).process(array);     
         Image resultImage = new Image(resArray, image.getType(), image);
         
         // configure calibration
@@ -127,5 +94,4 @@ public class ImageSlicer
         
         return resultImage;
     }
-
 }
